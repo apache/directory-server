40c7f966465d3597fc77a24596b2c53664e97b58
Alex Karasulu <akarasulu@apache.org>
2004-03-13 18:58:40 +0000

    Starting work on a snacc based stateful decoder.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directory/eve/trunk@9435 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eve/frontend/decoder/pojo-impl/src/java/org/apache/eve/decoder/impl/StatefulSnaccDecoder.java
@@ -0,0 +1,61 @@
+/*
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.eve.decoder.impl ;
+
+
+import org.apache.commons.codec.DecoderException ;
+import org.apache.commons.codec.stateful.DecoderCallback ;
+import org.apache.commons.codec.stateful.DecoderMonitor;
+import org.apache.commons.codec.stateful.StatefulDecoder ;
+
+
+/**
+ * Temporary stateful decoder based on the Snacc4j runtime.  This is here until
+ * the snickers code comes upto speed.
+ *
+ * @author <a href="mailto:directory-dev@incubator.apache.org">
+ * Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class StatefulSnaccDecoder implements StatefulDecoder
+{
+    /* (non-Javadoc)
+     * @see org.apache.commons.codec.stateful.StatefulDecoder#decode(
+     * java.lang.Object)
+     */
+    public void decode( Object arg0 ) throws DecoderException
+    {
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see org.apache.commons.codec.stateful.StatefulDecoder#setCallback(
+     * org.apache.commons.codec.stateful.DecoderCallback)
+     */
+    public void setCallback( DecoderCallback arg0 )
+    {
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see org.apache.commons.codec.stateful.StatefulDecoder#setDecoderMonitor(
+     * org.apache.commons.codec.stateful.DecoderMonitor)
+     */
+    public void setDecoderMonitor( DecoderMonitor arg0 )
+    {
+    }
+}
