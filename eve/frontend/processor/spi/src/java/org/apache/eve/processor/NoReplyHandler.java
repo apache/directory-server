4202428cdf89897b6547693639b9556202db538a
Alex Karasulu <akarasulu@apache.org>
2004-03-19 01:11:19 +0000

    playing around with old interfaces and ideas
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directory/eve/trunk@9597 13f79535-47bb-0310-9956-ffa450edef68

---
+++ eve/frontend/processor/spi/src/java/org/apache/eve/processor/NoReplyHandler.java
@@ -0,0 +1,40 @@
+/*
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.eve.processor ;
+
+
+import org.apache.ldap.common.message.Request ;
+
+
+/**
+ * Represents handlers that do not return a response to the client.
+ *
+ * @author <a href="mailto:aok123@bellsouth.net">Alex Karasulu</a>
+ * @author $Author: akarasulu $
+ * @version $Revision: 1.2 $
+ */
+public interface NoReplyHandler
+    extends RequestHandler
+{
+    /**
+     * Handles requests that do not reply to the requesting client with a
+     * response.
+     *
+     * @param request the request without a response.
+     */
+    void handle( Request request ) ;
+}
---
+++ eve/frontend/processor/spi/src/java/org/apache/eve/processor/SingleReplyHandler.java
@@ -0,0 +1,51 @@
+/*
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.eve.processor ;
+
+
+import org.apache.ldap.common.message.ResultResponse ;
+import org.apache.ldap.common.message.MessageTypeEnum ;
+import org.apache.ldap.common.message.SingleReplyRequest ;
+
+
+/**
+ * Request handler signature for those requests that generate a single response
+ * for a request.
+ *
+ * @author <a href="mailto:aok123@bellsouth.net">Alex Karasulu</a>
+ * @author $Author: akarasulu $
+ * @version $Revision: 1.2 $
+ */
+public interface SingleReplyHandler
+    extends RequestHandler
+{
+    /**
+     * Gets the response message type for this SingleReplyHandler.
+     *
+     * @return the MessageTypeEnum constant associated with this handler.
+     */
+    MessageTypeEnum getResponseType() ;
+
+    /**
+     * Handles a request that generates a sole response by returning the
+     * response object back to the caller.
+     *
+     * @param request the request to handle.
+     * @return the response to the request argument.
+     */
+    ResultResponse handle( SingleReplyRequest request ) ;
+}
