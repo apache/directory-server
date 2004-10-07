13bf06c984f5bd06787d5a2ca14fec7519569dc2
Alex Karasulu <akarasulu@apache.org>
2004-10-07 05:50:15 +0000

    correct package names after the move
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directory/eve/trunk@53940 13f79535-47bb-0310-9956-ffa450edef68

--- backend/core/src/java/org/apache/eve/schema/config/AttributeTypeConfig.java
+++ backend/core/src/java/org/apache/eve/schema/config/AttributeTypeConfig.java
@@ -14,7 +14,7 @@
  *   limitations under the License.
  *
  */
-package org.apache.eve.config;
+package org.apache.eve.schema.config;
 
 /**
  * Document me.
--- backend/core/src/java/org/apache/eve/schema/config/SyntaxCheckerConfig.java
+++ backend/core/src/java/org/apache/eve/schema/config/SyntaxCheckerConfig.java
@@ -14,7 +14,7 @@
  *   limitations under the License.
  *
  */
-package org.apache.eve.config;
+package org.apache.eve.schema.config;
 
 import org.apache.ldap.common.schema.SyntaxChecker;
 
