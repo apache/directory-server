542a6ca867b2a9181e242b9a5ccdec42d912a4dc
Alex Karasulu <akarasulu@apache.org>
2004-10-21 23:16:41 +0000

    Commit changes ...
     o added a new dn component normalizer implementation which uses registries
       to resolve the normalizer to use for the value while normalizing a name
       component's attribute value
     o added the set of normalizers for the core schema
     o added the set of comparators for the core schema
    
    Notes ...
    
     o these normalizers and comparators are really rough estimates for the
       various matching rules and can be tweeked as we go along
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directory/eve/trunk@55283 13f79535-47bb-0310-9956-ffa450edef68

---
+++ backend/core/src/java/org/apache/eve/schema/bootstrap/RegistryNameComponentNormalizer.java
@@ -0,0 +1,91 @@
+/*
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.eve.schema.bootstrap;
+
+
+import javax.naming.NamingException;
+
+import org.apache.ldap.common.schema.Normalizer;
+import org.apache.ldap.common.schema.AttributeType;
+import org.apache.ldap.common.name.NameComponentNormalizer;
+
+import org.apache.eve.schema.AttributeTypeRegistry;
+
+
+/**
+ * A DN Name component Normalizer which uses the bootstrap registries to find
+ * the appropriate normalizer for the attribute of the name component with which
+ * to normalize the name component value.
+ *
+ * @author <a href="mailto:directory-dev@incubator.apache.org">Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class RegistryNameComponentNormalizer implements NameComponentNormalizer
+{
+    /** the bootstrap registries used to dynamically resolve Normalizers */
+    private final BootstrapRegistries registries;
+
+
+    /**
+     * Creates a DN Name component Normalizer which uses the bootstrap
+     * registries to find the appropriate normalizer for the attribute of the
+     * name component with which to normalize the name component value.
+     *
+     * @param registries the bootstrap registries to use for resolution
+     */
+    public RegistryNameComponentNormalizer( BootstrapRegistries registries )
+    {
+        this.registries = registries;
+    }
+
+
+    /**
+     * @see NameComponentNormalizer#normalizeByName(String, String)
+     */
+    public String normalizeByName( String name, String value ) throws NamingException
+    {
+        return lookup( name ).normalize( value ).toString();
+    }
+
+
+    /**
+     * @see NameComponentNormalizer#normalizeByOid(String, String)
+     */
+    public String normalizeByOid( String oid, String value ) throws NamingException
+    {
+        return lookup( oid ).normalize( value ).toString();
+    }
+
+
+    /**
+     * Looks up the Normalizer to use for a name component using the attributeId
+     * for the name component.  First the attribute is resolved, then its
+     * equality matching rule is looked up.  The normalizer of that matching
+     * rule is returned.
+     *
+     * @param id the name or oid of the attribute in the name component to
+     * normalize the value of
+     * @return the Normalizer to use for normalizing the value of the attribute
+     * @throws NamingException if there are failures resolving the Normalizer
+     */
+    private Normalizer lookup( String id ) throws NamingException
+    {
+        AttributeTypeRegistry registry = registries.getAttributeTypeRegistry();
+        AttributeType type = registry.lookup( id );
+        return type.getEquality().getNormalizer();
+    }
+}
