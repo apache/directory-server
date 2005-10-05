6062fc03b8fd2153ed9073e8efd29efa6f776489
Stefan Zoerner <szoerner@apache.org>
2005-10-05 19:44:26 +0000

    Removed syntax errors from file apacheds-server.bat in order to resolve DIREVE-263 ("apacheds-server.bat of standalone goal fails on Windows platform").
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@295093 13f79535-47bb-0310-9956-ffa450edef68

--- main/src/main/resources/apacheds-server.bat
+++ main/src/main/resources/apacheds-server.bat
@@ -5,16 +5,19 @@ rem
 rem @..@ indicates that ant will parse this file and set the correct value.
 
 
-set MAIN_JAR="@APACHE_DS_MAIN_JAR@"
+set MAIN_JAR=@APACHE_DS_MAIN_JAR@
 
-set ARGS="-Xms128m -Xmx256m -jar %MAIN_JAR% apacheds-server.xml"
+set ARGS=-Xms128m -Xmx256m -jar %MAIN_JAR% apacheds-server.xml
 
 rem still needs to be constructed
 rem if someone uses windows and wants to make a duplicate of apacheds-server.sh, go for it.
 
 set CLASSPATH=%CLASSPATH%;.
 
+rem Create the logs dir, if it does not already exist
+if exist logs goto logsExists
 mkdir logs
+:logsExists
 
 java %ARGS%
 
