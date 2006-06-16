1385da93c8987cee6ce13d47c655701944fa5538
Alex Karasulu <akarasulu@apache.org>
2006-06-16 15:21:34 +0000

    Fixed responses to stored procedure requests which failed with exceptions.  What was happening is that a response was not returning an OID and this was causing the CommunicationExcpetion with the JNDI LDAP provider.  Now the request that is produced by the TwixTransformer is an instance of StoredProcedureRequest and it returns a StoredProcedureResponse when we invoke getResultResponse() on it.
    The result response object now is an instdance of StoredProcedureResponse as 
    opposed to a normal ExtendedResponseImpl instance.  This allows us to infer the
    OID and add that automatically.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/apacheds@414849 13f79535-47bb-0310-9956-ffa450edef68

--- protocol-ldap/src/main/java/org/apache/directory/server/ldap/support/ExtendedHandler.java
+++ protocol-ldap/src/main/java/org/apache/directory/server/ldap/support/ExtendedHandler.java
@@ -1,4 +1,4 @@
-/*
+    /*
  *   Copyright 2004 The Apache Software Foundation
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
@@ -100,9 +100,10 @@ public class ExtendedHandler implements MessageHandler
                 result.setResultCode( ResultCodeEnum.OTHER );
                 result.setErrorMessage( "Extended operation handler for the specified EXTENSION_OID (" + req.getOid()
                     + ") has failed to process your request:\n" + ExceptionUtils.getStackTrace( e ) );
-                ( ( ExtendedResponse ) req.getResultResponse() ).setResponse( new byte[0] );
+                ExtendedResponse resp = ( ExtendedResponse ) req.getResultResponse();
+                resp.setResponse( new byte[0] );
                 session.write( req.getResultResponse() );
-            }
+            } 
         }
     }
 }
