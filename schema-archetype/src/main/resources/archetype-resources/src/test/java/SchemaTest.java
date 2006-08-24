b6181cbfedcc865703363f3d298d09d6a9d13a05
Alex Karasulu <akarasulu@apache.org>
2006-08-24 19:38:08 +0000

    filling in test case methods to test and use the new schema
    git-svn-id: https://svn.apache.org/repos/asf/directory/branches/apacheds/1.0@434478 13f79535-47bb-0310-9956-ffa450edef68

--- schema-archetype/src/main/resources/archetype-resources/src/test/java/SchemaTest.java
+++ schema-archetype/src/main/resources/archetype-resources/src/test/java/SchemaTest.java
@@ -20,12 +20,19 @@
 package ${groupId};
 
 
-import javax.naming.directory.*;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 
 import org.apache.directory.server.unit.AbstractServerTest;
 
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 
 /**
@@ -49,8 +56,12 @@ public class SchemaTest extends AbstractServerTest
         // Setup our schema to load       
         // -------------------------------------------------------------------
 
-        // todo 
-
+        Set schemas = configuration.getBootstrapSchemas();
+        Set newset = new HashSet();
+        newset.addAll( schemas );
+        newset.add( new CarSchema() );
+        configuration.setBootstrapSchemas( newset );
+        
         super.setUp();
 
         // -------------------------------------------------------------------
@@ -83,6 +94,55 @@ public class SchemaTest extends AbstractServerTest
      */
     public void testSchemaPresence() throws NamingException
     {
+        Attributes attrs = ctx.getAttributes( "cn=schema", new String[] { "attributeTypes", "objectClasses" } );
+
+        // -------------------------------------------------------------------
+        // Check that our 3 attributeTypes are present in cn=schema,ou=system
+        // -------------------------------------------------------------------
+        
+        Attribute attributeTypes = attrs.get( "attributeTypes" );
+        boolean isCarMakeFound = false;
+        boolean isCarModelFound = false;
+        boolean isCarYearFound = false;
+        for ( NamingEnumeration ii = attributeTypes.getAll(); ii.hasMore(); /**/ )
+        {
+            String id = ( String ) ii.next();
+
+            if ( id.indexOf( "1.2.6.1.4.1.18060.1.1.1.3.1001" ) != -1 )
+            {
+                isCarMakeFound = true;
+            }
+            
+            if ( id.indexOf( "1.2.6.1.4.1.18060.1.1.1.3.1002" ) != -1 )
+            {
+                isCarModelFound = true;
+            }
+            
+            if ( id.indexOf( "1.2.6.1.4.1.18060.1.1.1.3.1003" ) != -1 )
+            {
+                isCarYearFound = true;
+            }
+        }
+        assertTrue( isCarMakeFound );
+        assertTrue( isCarModelFound );
+        assertTrue( isCarYearFound );
+        
+        // -------------------------------------------------------------------
+        // Check that our 1 objectClass is present in cn=schema,ou=system
+        // -------------------------------------------------------------------
+        
+        Attribute objectClasses = attrs.get( "objectClasses" );
+        boolean isCarFound = false;
+        for ( NamingEnumeration ii = objectClasses.getAll(); ii.hasMore(); /**/ )
+        {
+            String id = ( String ) ii.next();
+
+            if ( id.indexOf( "1.2.6.1.4.1.18060.1.1.1.4.1001" ) != -1 )
+            {
+                isCarFound = true;
+            }
+        }
+        assertTrue( isCarFound );
     }
 
 
@@ -91,5 +151,16 @@ public class SchemaTest extends AbstractServerTest
      */
     public void testAddCar() throws NamingException
     {
+        Attributes newCar = new BasicAttributes( "objectClass", "car", true );
+        newCar.put( "carMake", "Porsche" );
+        newCar.put( "carModel", "Boxter" );
+        newCar.put( "carYear", "2006" );
+        ctx.createSubcontext( "carMake=Porsche", newCar );
+        
+        Attributes porsche = ctx.getAttributes( "carMake=Porsche" );
+        assertNotNull( porsche );
+        assertEquals( "Porsche", porsche.get( "carMake" ).get() );
+        assertEquals( "Boxter", porsche.get( "carModel" ).get() );
+        assertEquals( "2006", porsche.get( "carYear" ).get() );
     }
 }
