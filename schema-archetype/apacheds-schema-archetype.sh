994cb7700388c431bd21d30eff9379e5d0d080ba
Alex Karasulu <akarasulu@apache.org>
2006-08-24 19:40:09 +0000

    adding simple shell script that shows how to run this archetype
    git-svn-id: https://svn.apache.org/repos/asf/directory/branches/apacheds/1.0@434479 13f79535-47bb-0310-9956-ffa450edef68

---
+++ schema-archetype/apacheds-schema-archetype.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mvn archetype:create -DarchetypeGroupId=org.apache.directory.server -DarchetypeArtifactId=apacheds-schema-archetype -DarchetypeVersion=1.0-RC4-SNAPSHOT -DgroupId=$1 -DartifactId=$2
+
