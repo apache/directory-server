a4f9654c18ec191f5e1038bbd6f74e35666eea8e
Alex Karasulu <akarasulu@apache.org>
2006-09-04 00:52:36 +0000

    changing version on shell scripts to invoke archetypes
    git-svn-id: https://svn.apache.org/repos/asf/directory/branches/apacheds/1.0@439889 13f79535-47bb-0310-9956-ffa450edef68

--- testcase-archetype/apacheds-testcase-archetype.sh
+++ testcase-archetype/apacheds-testcase-archetype.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn archetype:create -DarchetypeGroupId=org.apache.directory.server -DarchetypeArtifactId=apacheds-testcase-archetype -DarchetypeVersion=1.0-RC4-SNAPSHOT -DgroupId=$1 -DartifactId=$2
+mvn archetype:create -DarchetypeGroupId=org.apache.directory.server -DarchetypeArtifactId=apacheds-testcase-archetype -DarchetypeVersion=1.0-RC4 -DgroupId=$1 -DartifactId=$2
 
