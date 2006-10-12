5d3f28a13759b05d8275e7720532d6bc03b235b4
Emmanuel Lecharny <elecharny@apache.org>
2006-10-12 17:35:47 +0000

    Changed the number of loop in order to avoid a very long running test
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/shared@463338 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/test/java/org/apache/directory/shared/ldap/codec/bind/BindRequestPerfTest.java
+++ ldap/src/test/java/org/apache/directory/shared/ldap/codec/bind/BindRequestPerfTest.java
@@ -85,7 +85,7 @@ public class BindRequestPerfTest extends TestCase
         // Decode the BindRequest PDU
         try
         {
-            int nbLoops = 1000000;
+            int nbLoops = 10;
             long t0 = System.currentTimeMillis();
             
             for ( int i = 0; i < nbLoops; i++ )
@@ -157,7 +157,7 @@ public class BindRequestPerfTest extends TestCase
     public void testEncodeBindRequestPerf() throws Exception
     {
         LdapDN name = new LdapDN( "uid=akarasulu,dc=example,dc=com" );
-        int nbLoops = 10000000;
+        int nbLoops = 10;
         long t0 = System.currentTimeMillis();
         ByteBuffer bb=null;
         
