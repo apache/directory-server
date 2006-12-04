7d465b544b8c20922d43437f867ce0537a21cbe6
John E. Conlon <jconlon@apache.org>
2006-12-04 19:42:17 +0000

    updated name of uberjar
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/apacheds@482315 13f79535-47bb-0310-9956-ffa450edef68

--- server-main/apacheds.sh
+++ server-main/apacheds.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ -e target/apacheds-server-main-1.1.0-SNAPSHOT-app.jar ] ; then
+if [ -e target/apacheds-server-main-1.5.0-SNAPSHOT-app.jar ] ; then
   echo uber jar exists
 else
   echo uber jar not found need to build it
   mvn clean assembly:assembly
 fi
 
-java -Dlog4j.configuration=file://$(pwd)/log4j.properties -jar target/apacheds-server-main-1.1.0-SNAPSHOT-app.jar server.xml 
+java -Dlog4j.configuration=file://$(pwd)/log4j.properties -jar target/apacheds-server-main-1.5.0-SNAPSHOT-app.jar server.xml 
