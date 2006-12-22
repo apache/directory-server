47c98ba5e0e122be0468b68ec46762028a53c77d
Emmanuel Lecharny <elecharny@apache.org>
2006-12-22 19:56:44 +0000

    Fixed Jira DIRSERVER-804
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/apacheds@489742 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/directory/server/core/operational/OperationalAttributeService.java
+++ core/src/main/java/org/apache/directory/server/core/operational/OperationalAttributeService.java
@@ -379,17 +379,22 @@ public class OperationalAttributeService extends BaseInterceptor
             if ( attr != null )
             {
                 LdapDN creatorsName = new LdapDN( ( String ) attr.get() );
-                attr.set( 0, denormalizeTypes( creatorsName ).getUpName() );
+                
+                attr.clear();
+                attr.add( denormalizeTypes( creatorsName ).getUpName() );
             }
             
             type = null;
             type = registry.lookup( "modifiersName" );
             attr = null;
             attr = AttributeUtils.getAttribute( entry, type );
+            
             if ( attr != null )
             {
                 LdapDN modifiersName = new LdapDN( ( String ) attr.get() );
-                attr.set( 0, denormalizeTypes( modifiersName ).getUpName() );
+
+                attr.clear();
+                attr.add( denormalizeTypes( modifiersName ).getUpName() );
             }
         }
     }
