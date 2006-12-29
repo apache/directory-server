6a21cc8776aaf259b12a313c11ee8a3843bb9745
Emmanuel Lecharny <elecharny@apache.org>
2006-12-29 15:49:58 +0000

    The normalizer is now called with the String type
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/shared@491034 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/schema/DeepTrimNormalizer.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/DeepTrimNormalizer.java
@@ -6,19 +6,21 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.directory.shared.ldap.schema;
 
+
+import java.io.IOException;
 
 import javax.naming.NamingException;
 
@@ -29,26 +31,32 @@ import org.apache.directory.shared.ldap.util.StringTools;
  * Normalizer which trims down whitespace replacing multiple whitespace
  * characters on the edges and within the string with a single space character
  * thereby preserving tokenization order.
- * 
+ *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$
  */
 public class DeepTrimNormalizer implements Normalizer
 {
-    public Object normalize( Object value ) throws NamingException
-    {
-        if ( value == null )
-        {
-            return null;
-        }
+   static final long serialVersionUID = 1L;
 
-        if ( value instanceof byte[] )
-        {
-            return StringTools.deepTrim( StringTools.utf8ToString( ( byte[] ) value ) );
-        }
-        else
-        {
-            return StringTools.deepTrim( ( String ) value );
-        }
-    }
+   public Object normalize( Object value ) throws NamingException
+   {
+       try
+       {
+           if ( value instanceof byte[] )
+           {
+               return PrepareString.normalize( StringTools.utf8ToString( ( byte[] ) value ), 
+                   PrepareString.StringType.DIRECTORY_STRING );
+           }
+           else
+           {
+               return PrepareString.normalize( ( String ) value,
+                   PrepareString.StringType.DIRECTORY_STRING );
+           }
+       }
+       catch ( IOException ioe )
+       {
+           throw new NamingException( "Invalid value : " + value );
+       }
+   }
 }
