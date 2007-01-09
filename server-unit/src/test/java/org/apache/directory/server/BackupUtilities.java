b39afbd3cdf298b4614920d2f254175c738a6a27
Alex Karasulu <akarasulu@apache.org>
2007-01-09 01:01:00 +0000

    keep this stuff quiet by logging to std out instead of System.out
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/apacheds@494285 13f79535-47bb-0310-9956-ffa450edef68

--- server-unit/src/test/java/org/apache/directory/server/BackupUtilities.java
+++ server-unit/src/test/java/org/apache/directory/server/BackupUtilities.java
@@ -24,16 +24,22 @@ import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 import javax.naming.ldap.LdapContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class BackupUtilities
 {
+    private static final Logger log = LoggerFactory.getLogger( BackupUtilities.class );
+
+
     public static void backupDeleted( LdapContext ctx, Name deletedEntryName, Name operationPrincipal, Attributes deletedEntry ) throws NamingException
     {
-        System.out.println( "User \"" + operationPrincipal + "\" has deleted entry \"" + deletedEntryName + "\"" );
-        System.out.println( "Entry content was: " + deletedEntry );
+        log.info( "User \"" + operationPrincipal + "\" has deleted entry \"" + deletedEntryName + "\"" );
+        log.info( "Entry content was: " + deletedEntry );
         LdapContext backupCtx = ( LdapContext ) ctx.lookup( "ou=backupContext,ou=system" );
         String deletedEntryRdn = deletedEntryName.get( deletedEntryName.size() - 1 );
         backupCtx.createSubcontext( deletedEntryRdn, deletedEntry );
-        System.out.println( "Backed up deleted entry to \"" + ( ( LdapContext ) backupCtx.lookup( deletedEntryRdn ) ).getNameInNamespace() + "\"" );
+        log.info( "Backed up deleted entry to \"" + ( ( LdapContext ) backupCtx.lookup( deletedEntryRdn ) ).getNameInNamespace() + "\"" );
     }
 }
