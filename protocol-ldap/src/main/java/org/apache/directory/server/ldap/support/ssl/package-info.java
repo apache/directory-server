e593d481ec75785005d46e74a49dbfd51b9693c7
Michael E. Rodriguez <erodriguez@apache.org>
2007-03-10 01:41:02 +0000

    Added package comment file (package-info.java) to the SSL package now that it is part of the LDAP protocol provider.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-sasl-branch@516637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ protocol-ldap/src/main/java/org/apache/directory/server/ldap/support/ssl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains support for SSL with the LDAP protocol (LDAPS).
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+package org.apache.directory.server.ldap.support.ssl;
