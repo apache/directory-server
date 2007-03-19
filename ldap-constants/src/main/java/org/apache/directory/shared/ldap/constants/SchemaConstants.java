9aed1f753160a0daba1e92eda05adc7746d28c3f
Emmanuel Lecharny <elecharny@apache.org>
2007-03-19 16:09:40 +0000

    Some more constant migration from code to ShemaConstants
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@519986 13f79535-47bb-0310-9956-ffa450edef68

--- ldap-constants/src/main/java/org/apache/directory/shared/ldap/constants/SchemaConstants.java
+++ ldap-constants/src/main/java/org/apache/directory/shared/ldap/constants/SchemaConstants.java
@@ -30,24 +30,39 @@ package org.apache.directory.shared.ldap.constants;
 public class SchemaConstants
 {
     // ---- ObjectClasses -----------------------------------------------------
-    public static final String SUBENTRY_OC = "subentry";
-    
     // Top
     public static final String TOP_OC = "top";
     public static final String TOP_OC_OID = "2.5.6.0";
+    
+    // OrganizationalUnit
+    public static final String ORGANIZATIONAL_UNIT_OC = "organizationalUnit";
+    public static final String ORGANIZATIONAL_UNIT_OC_OID = "2.5.6.5";
+
+    // Person
+    public static final String PERSON_OC = "person";
+    public static final String PERSON_OC_OID = "2.5.6.6";
+    
+    // OrganizationalPerson
+    public static final String ORGANIZATIONAL_PERSON_OC = "organizationalPerson";
+    public static final String ORGANIZATIONAL_PERSON_OC_OID = "2.5.6.7";
+
+    // Subentry
+    public static final String SUBENTRY_OC = "subentry";
+    public static final String SUBENTRY_OC_OID = "2.5.17.0";
     
     // Subschema
     public static final String SUBSCHEMA_OC = "subschema";
     public static final String SUBSCHEMA_OC_OID = "2.5.20.1";
     
+    // InetOrgPerson
+    public static final String INET_ORG_PERSON_OC = "inetOrgPerson";
+    public static final String INET_ORG_PERSON_OC_OID = "2.16.840.1.113730.3.2.2";
+    
+
     // ExtensibleObject
     public static final String EXTENSIBLE_OBJECT_OC = "extensibleObject";
     public static final String EXTENSIBLE_OBJECT_OC_OID = "1.3.6.1.4.1.1466.101.120.111";
 
-    // OrganizationalUnit
-    public static final String ORGANIZATIONAL_UNIT_OC = "organizationalUnit";
-    public static final String ORGANIZATIONAL_UNIT_OC_OID = "2.5.6.5";
-    
     // ---- AttributeTypes ----------------------------------------------------
     // ObjectClass
     public static final String OBJECT_CLASS_AT = "objectClass";
@@ -58,11 +73,20 @@ public class SchemaConstants
     public static final String COMMON_NAME_AT = "commonName";
     public static final String CN_AT_OID = "2.5.4.3";
     
+    // Sn
+    public static final String SN_AT = "sn";
+    public static final String SURNAME_AT = "surname";
+    public static final String SN_AT_OID = "2.5.4.4";
+    
     // Ou
     public static final String OU_AT = "ou";
     public static final String ORGANIZATIONAL_UNIT_NAME_AT = "organizationalUnitName";
     public static final String OU_AT_OID = "2.5.4.11";
     
+    // UserPassword
+    public static final String USER_PASSWORD_AT = "userPassword";
+    public static final String USER_PASSWORD_AT_OID = "2.5.4.35";
+
     // CreateTimestamp
     public static final String CREATE_TIMESTAMP_AT = "createTimestamp";
     public static final String CREATE_TIMESTAMP_AT_OID = "2.5.18.1";
@@ -111,7 +135,20 @@ public class SchemaConstants
     public static final String STRUCTURAL_OBJECT_CLASS_AT = "structuralObjectClass";
     public static final String STRUCTURAL_OBJECT_CLASS_AT_OID = "2.5.21.9";
     
+    // Uid
+    public static final String UID_AT = "uid";
+    public static final String USER_ID_AT = "userid";
+    public static final String UID_AT_OID = "0.9.2342.19200300.100.1.1";
+
+    // UidObject
+    public static final String UID_OBJECT_AT = "uidObject";
+    public static final String UID_OBJECT_AT_OID = "1.3.6.1.1.3.1";
+
     // LdapSyntaxes
     public static final String LDAP_SYNTAXES_AT = "ldapSyntaxes";
     public static final String LDAP_SYNTAXES_AT_OID = "1.3.6.1.4.1.1466.101.120.16";
+
+    // DisplayName
+    public static final String DISPLAY_NAME_AT = "displayName";
+    public static final String DISPLAY_NAME_AT_OID = "2.16.840.1.113730.3.1.241";
 }
