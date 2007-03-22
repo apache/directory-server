6ba3378a8a6bb5fdcd7de37415884cd3a2921ecb
Emmanuel Lecharny <elecharny@apache.org>
2007-03-22 18:10:36 +0000

    Fixed DIRSERVER-777 : the ads version is now 1.0.2-SNAPSHOT, not 1.0-RC4
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/1.0@521389 13f79535-47bb-0310-9956-ffa450edef68

--- schema-archetype/apacheds-schema-archetype.sh
+++ schema-archetype/apacheds-schema-archetype.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn archetype:create -DarchetypeGroupId=org.apache.directory.server -DarchetypeArtifactId=apacheds-schema-archetype -DarchetypeVersion=1.0-RC4 -DgroupId=$1 -DartifactId=$2
+mvn archetype:create -DarchetypeGroupId=org.apache.directory.server -DarchetypeArtifactId=apacheds-schema-archetype -DarchetypeVersion=1.0.2-SNAPSHOT -DgroupId=$1 -DartifactId=$2
 
