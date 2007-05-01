2344f449441bc6f904ef62d9f8588d8a492ae34f
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-01 05:00:11 +0000

    o  Addition of support for DES3-CBC-SHA1-KD encryption type. o  Unit tests with key derivation test vectors from RFC 3961. o  Removal of unused DES3-based encryption types.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/kerberos-encryption-types@533941 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/crypto/encryption/Des3CbcSha1KdEncryption.java
@@ -0,0 +1,293 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.server.kerberos.shared.crypto.encryption;
+
+
+import java.security.GeneralSecurityException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.directory.server.kerberos.shared.exceptions.ErrorType;
+import org.apache.directory.server.kerberos.shared.exceptions.KerberosException;
+import org.apache.directory.server.kerberos.shared.messages.value.EncryptedData;
+import org.apache.directory.server.kerberos.shared.messages.value.EncryptionKey;
+
+
+/**
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+public class Des3CbcSha1KdEncryption extends EncryptionEngine
+{
+    public EncryptionType getEncryptionType()
+    {
+        return EncryptionType.DES3_CBC_SHA1_KD;
+    }
+
+
+    public int getConfounderLength()
+    {
+        return 8;
+    }
+
+
+    public int getChecksumLength()
+    {
+        return 20;
+    }
+
+
+    public byte[] calculateChecksum( byte[] data, byte[] key )
+    {
+        try
+        {
+            SecretKey sk = new SecretKeySpec( key, "DESede" );
+
+            Mac mac = Mac.getInstance( "HmacSHA1" );
+            mac.init( sk );
+
+            return mac.doFinal( data );
+        }
+        catch ( GeneralSecurityException nsae )
+        {
+            nsae.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public byte[] getDecryptedData( EncryptionKey key, EncryptedData data ) throws KerberosException
+    {
+        byte[] usageKe =
+            { ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x01, ( byte ) 0xaa };
+        byte[] usageKi =
+            { ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x01, ( byte ) 0x55 };
+
+        byte[] Ke = deriveKey( key.getKeyValue(), usageKe, 64, 168 );
+        byte[] Ki = deriveKey( key.getKeyValue(), usageKi, 64, 168 );
+
+        byte[] encryptedData = data.getCipherText();
+
+        // extract the old checksum
+        byte[] oldChecksum = new byte[getChecksumLength()];
+        System
+            .arraycopy( encryptedData, encryptedData.length - getChecksumLength(), oldChecksum, 0, oldChecksum.length );
+
+        // remove trailing checksum
+        encryptedData = removeTrailingBytes( encryptedData, 0, getChecksumLength() );
+
+        // decrypt the data
+        byte[] decryptedData = decrypt( encryptedData, Ke );
+
+        // remove leading confounder
+        byte[] withoutConfounder = removeLeadingBytes( decryptedData, getConfounderLength(), 0 );
+
+        // calculate a new checksum
+        byte[] newChecksum = calculateChecksum( decryptedData, Ki );
+
+        // compare checksums
+        if ( !Arrays.equals( oldChecksum, newChecksum ) )
+        {
+            throw new KerberosException( ErrorType.KRB_AP_ERR_BAD_INTEGRITY );
+        }
+
+        return withoutConfounder;
+    }
+
+
+    public EncryptedData getEncryptedData( EncryptionKey key, byte[] plainText )
+    {
+        byte[] usageKe =
+            { ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x01, ( byte ) 0xaa };
+        byte[] usageKi =
+            { ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x01, ( byte ) 0x55 };
+
+        byte[] Ke = deriveKey( key.getKeyValue(), usageKe, 64, 168 );
+        byte[] Ki = deriveKey( key.getKeyValue(), usageKi, 64, 168 );
+
+        // build the ciphertext structure
+        byte[] conFounder = getRandomBytes( getConfounderLength() );
+        byte[] paddedPlainText = padString( plainText );
+        byte[] dataBytes = concatenateBytes( conFounder, paddedPlainText );
+        byte[] checksumBytes = calculateChecksum( dataBytes, Ki );
+
+        //byte[] encryptedData = encrypt( paddedDataBytes, key.getKeyValue() );
+        byte[] encryptedData = encrypt( dataBytes, Ke );
+
+        byte[] cipherText = concatenateBytes( encryptedData, checksumBytes );
+
+        return new EncryptedData( getEncryptionType(), key.getKeyVersion(), cipherText );
+    }
+
+
+    /**
+     * Derived Key = DK(Base Key, Well-Known Constant)
+     * DK(Key, Constant) = random-to-key(DR(Key, Constant))
+     * DR(Key, Constant) = k-truncate(E(Key, Constant, initial-cipher-state))
+     */
+    protected byte[] deriveKey( byte[] baseKey, byte[] usage, int n, int k )
+    {
+        byte[] result = deriveRandom( baseKey, usage, n, k );
+        result = randomToKey( result );
+
+        return result;
+    }
+
+
+    protected byte[] randomToKey( byte[] seed )
+    {
+        int kBytes = 24;
+        byte[] result = new byte[kBytes];
+
+        byte[] fillingKey = new byte[0];
+
+        int pos = 0;
+
+        for ( int i = 0; i < kBytes; i++ )
+        {
+            if ( pos < fillingKey.length )
+            {
+                result[i] = fillingKey[pos];
+                pos++;
+            }
+            else
+            {
+                fillingKey = getBitGroup( seed, i / 8 );
+                fillingKey = setParity( fillingKey );
+                pos = 0;
+                result[i] = fillingKey[pos];
+                pos++;
+            }
+        }
+
+        return result;
+    }
+
+
+    protected byte[] getBitGroup( byte[] seed, int group )
+    {
+        int srcPos = group * 7;
+
+        byte[] result = new byte[7];
+
+        System.arraycopy( seed, srcPos, result, 0, 7 );
+
+        return result;
+    }
+
+
+    protected byte[] setParity( byte[] in )
+    {
+        byte[] expandedIn = new byte[8];
+
+        System.arraycopy( in, 0, expandedIn, 0, in.length );
+
+        setBit( expandedIn, 62, getBit( in, 7 ) );
+        setBit( expandedIn, 61, getBit( in, 15 ) );
+        setBit( expandedIn, 60, getBit( in, 23 ) );
+        setBit( expandedIn, 59, getBit( in, 31 ) );
+        setBit( expandedIn, 58, getBit( in, 39 ) );
+        setBit( expandedIn, 57, getBit( in, 47 ) );
+        setBit( expandedIn, 56, getBit( in, 55 ) );
+
+        byte[] out = new byte[8];
+
+        int bitCount = 0;
+        int index = 0;
+
+        for ( int i = 0; i < 64; i++ )
+        {
+            if ( ( i + 1 ) % 8 == 0 )
+            {
+                if ( bitCount % 2 == 0 )
+                {
+                    setBit( out, i, 1 );
+                }
+
+                index++;
+                bitCount = 0;
+            }
+            else
+            {
+                int val = getBit( expandedIn, index );
+                boolean bit = val > 0;
+
+                if ( bit )
+                {
+                    setBit( out, i, val );
+                    bitCount++;
+                }
+
+                index++;
+            }
+        }
+
+        return out;
+    }
+
+
+    public byte[] encrypt( byte[] plainText, byte[] keyBytes )
+    {
+        return processCipher( true, plainText, keyBytes );
+    }
+
+
+    public byte[] decrypt( byte[] cipherText, byte[] keyBytes )
+    {
+        return processCipher( false, cipherText, keyBytes );
+    }
+
+
+    private byte[] processCipher( boolean isEncrypt, byte[] data, byte[] keyBytes )
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance( "DESede/CBC/NoPadding" );
+            SecretKey key = new SecretKeySpec( keyBytes, "DESede" );
+
+            byte[] iv = new byte[]
+                { ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00, ( byte ) 0x00,
+                    ( byte ) 0x00, ( byte ) 0x00 };
+            AlgorithmParameterSpec paramSpec = new IvParameterSpec( iv );
+
+            if ( isEncrypt )
+            {
+                cipher.init( Cipher.ENCRYPT_MODE, key, paramSpec );
+            }
+            else
+            {
+                cipher.init( Cipher.DECRYPT_MODE, key, paramSpec );
+            }
+
+            return cipher.doFinal( data );
+        }
+        catch ( GeneralSecurityException nsae )
+        {
+            nsae.printStackTrace();
+            return null;
+        }
+    }
+}
