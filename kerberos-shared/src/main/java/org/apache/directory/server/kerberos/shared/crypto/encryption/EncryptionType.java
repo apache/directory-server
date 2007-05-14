8c257943cba4f976d6b0aa2988c789b9c143fe85
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-14 04:48:34 +0000

    Javadocs and minor warning clean-up.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/kerberos-encryption-types@537710 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/crypto/encryption/EncryptionType.java
+++ kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/crypto/encryption/EncryptionType.java
@@ -26,45 +26,142 @@ import java.util.List;
 
 
 /**
+ * A type-safe enumeration of Kerberos encryption types.
+ * 
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
  */
 public final class EncryptionType implements Comparable
 {
     /**
-     * Enumeration elements are constructed once upon class loading.
-     * Order of appearance here determines the order of compareTo.
+     * The "unknown" encryption type.
      */
     public static final EncryptionType UNKNOWN = new EncryptionType( -1, "UNKNOWN" );
+
+    /**
+     * The "null" encryption type.
+     */
     public static final EncryptionType NULL = new EncryptionType( 0, "NULL" );
+
+    /**
+     * The des-cbc-crc encryption type.
+     */
     public static final EncryptionType DES_CBC_CRC = new EncryptionType( 1, "des-cbc-crc" );
+
+    /**
+     * The des-cbc-md4 encryption type.
+     */
     public static final EncryptionType DES_CBC_MD4 = new EncryptionType( 2, "des-cbc-md4" );
+
+    /**
+     * The des-cbc-md5 encryption type.
+     */
     public static final EncryptionType DES_CBC_MD5 = new EncryptionType( 3, "des-cbc-md5" );
+
+    /**
+     * The reserved (4) encryption type.
+     */
     public static final EncryptionType RESERVED4 = new EncryptionType( 4, "[reserved]" );
+
+    /**
+     * The des3-cbc-md5 encryption type.
+     */
     public static final EncryptionType DES3_CBC_MD5 = new EncryptionType( 5, "des3-cbc-md5" );
+
+    /**
+     * The reserved (6) encryption type.
+     */
     public static final EncryptionType RESERVED6 = new EncryptionType( 6, "[reserved]" );
+
+    /**
+     * The des3-cbc-sha1 encryption type.
+     */
     public static final EncryptionType DES3_CBC_SHA1 = new EncryptionType( 7, "des3-cbc-sha1" );
+
+    /**
+     * The dsaWithSHA1-CmsOID encryption type.
+     */
     public static final EncryptionType DSAWITHSHA1_CMSOID = new EncryptionType( 9, "dsaWithSHA1-CmsOID" );
+
+    /**
+     * The md5WithRSAEncryption-CmsOID encryption type.
+     */
     public static final EncryptionType MD5WITHRSAENCRYPTION_CMSOID = new EncryptionType( 10,
         "md5WithRSAEncryption-CmsOID" );
+
+    /**
+     * The sha1WithRSAEncryption-CmsOID encryption type.
+     */
     public static final EncryptionType SHA1WITHRSAENCRYPTION_CMSOID = new EncryptionType( 11,
         "sha1WithRSAEncryption-CmsOID" );
+
+    /**
+     * The rc2CBC-EnvOID encryption type.
+     */
     public static final EncryptionType RC2CBC_ENVOID = new EncryptionType( 12, "rc2CBC-EnvOID" );
+
+    /**
+     * The rsaEncryption-EnvOID encryption type.
+     */
     public static final EncryptionType RSAENCRYPTION_ENVOID = new EncryptionType( 13, "rsaEncryption-EnvOID" );
+
+    /**
+     * The rsaES-OAEP-ENV-OID encryption type.
+     */
     public static final EncryptionType RSAES_OAEP_ENV_OID = new EncryptionType( 14, "rsaES-OAEP-ENV-OID" );
+
+    /**
+     * The des-ede3-cbc-Env-OID encryption type.
+     */
     public static final EncryptionType DES_EDE3_CBC_ENV_OID = new EncryptionType( 15, "des-ede3-cbc-Env-OID" );
+
+    /**
+     * The des3-cbc-sha1-kd encryption type.
+     */
     public static final EncryptionType DES3_CBC_SHA1_KD = new EncryptionType( 16, "des3-cbc-sha1-kd" );
+
+    /**
+     * The aes128-cts-hmac-sha1-96 encryption type.
+     */
     public static final EncryptionType AES128_CTS_HMAC_SHA1_96 = new EncryptionType( 17, "aes128-cts-hmac-sha1-96" );
+
+    /**
+     * The aes256-cts-hmac-sha1-96 encryption type.
+     */
     public static final EncryptionType AES256_CTS_HMAC_SHA1_96 = new EncryptionType( 18, "aes256-cts-hmac-sha1-96" );
+
+    /**
+     * The rc4-hmac encryption type.
+     */
     public static final EncryptionType RC4_HMAC = new EncryptionType( 23, "rc4-hmac" );
+
+    /**
+     * The rc4-hmac-exp encryption type.
+     */
     public static final EncryptionType RC4_HMAC_EXP = new EncryptionType( 24, "rc4-hmac-exp" );
+
+    /**
+     * The subkey-keymaterial encryption type.
+     */
     public static final EncryptionType SUBKEY_KEYMATERIAL = new EncryptionType( 65, "subkey-keymaterial" );
+
+    /**
+     * The rc4-md4 encryption type.
+     */
     public static final EncryptionType RC4_MD4 = new EncryptionType( -128, "rc4-md4" );
+
+    /**
+     * The c4-hmac-old encryption type.
+     */
     public static final EncryptionType RC4_HMAC_OLD = new EncryptionType( -133, "rc4-hmac-old" );
+
+    /**
+     * The rc4-hmac-old-exp encryption type.
+     */
     public static final EncryptionType RC4_HMAC_OLD_EXP = new EncryptionType( -135, "rc4-hmac-old-exp" );
 
     /**
-     * These two lines are all that's necessary to export a List of VALUES.
+     * Array for building a List of VALUES.
      */
     private static final EncryptionType[] values =
         { UNKNOWN, NULL, DES_CBC_CRC, DES_CBC_MD4, DES_CBC_MD5, RESERVED4, DES3_CBC_MD5, RESERVED6, DES3_CBC_SHA1,
@@ -73,22 +170,38 @@ public final class EncryptionType implements Comparable
             AES256_CTS_HMAC_SHA1_96, RC4_HMAC, RC4_HMAC_EXP, SUBKEY_KEYMATERIAL, RC4_MD4, RC4_HMAC_OLD,
             RC4_HMAC_OLD_EXP };
 
+    /**
+     * A List of all the encryption type constants.
+     */
     public static final List<EncryptionType> VALUES = Collections.unmodifiableList( Arrays.asList( values ) );
 
+    /**
+     * The name of the encryption type.
+     */
     private final String name;
+
+    /**
+     * The value/code for the encryption type.
+     */
     private final int ordinal;
 
 
     /**
      * Private constructor prevents construction outside of this class.
      */
-    private EncryptionType(int ordinal, String name)
+    private EncryptionType( int ordinal, String name )
     {
         this.ordinal = ordinal;
         this.name = name;
     }
 
 
+    /**
+     * Returns the encryption type when specified by its ordinal.
+     *
+     * @param type
+     * @return The encryption type.
+     */
     public static EncryptionType getTypeByOrdinal( int type )
     {
         for ( int ii = 0; ii < values.length; ii++ )
@@ -103,12 +216,22 @@ public final class EncryptionType implements Comparable
     }
 
 
+    /**
+     * Returns the number associated with this encryption type.
+     *
+     * @return The encryption type number.
+     */
     public int getOrdinal()
     {
         return ordinal;
     }
 
 
+    /**
+     * Returns the name associated with this encryption type.
+     *
+     * @return The name.
+     */
     public String getName()
     {
         return name;
--- kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/exceptions/ErrorType.java
+++ kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/exceptions/ErrorType.java
@@ -26,103 +26,364 @@ import java.util.List;
 
 
 /**
- * Type safe enumeration of Kerberos error types
+ * A type-safe enumeration of Kerberos error types.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
  */
 public final class ErrorType implements Comparable
 {
-    /*
-     * Enumeration elements are constructed once upon class loading.
-     * Order of appearance here determines the order of compareTo.
+    /**
+     * No error.
      */
     public static final ErrorType KDC_ERR_NONE = new ErrorType( 0, "No error" );
+
+    /**
+     * Client's entry in database expired.
+     */
     public static final ErrorType KDC_ERR_NAME_EXP = new ErrorType( 1, "Client's entry in database expired" );
+
+    /**
+     * Server's entry in database has expired.
+     */
     public static final ErrorType KDC_ERR_SERVICE_EXP = new ErrorType( 2, "Server's entry in database has expired" );
+
+    /**
+     * Requested protocol version number not supported.
+     */
     public static final ErrorType KDC_ERR_BAD_PVNO = new ErrorType( 3,
         "Requested protocol version number not supported" );
+
+    /**
+     * Client's key encrypted in old master key.
+     */
     public static final ErrorType KDC_ERR_C_OLD_MAST_KVNO = new ErrorType( 4,
         "Client's key encrypted in old master key" );
+
+    /**
+     * Server's key encrypted in old master key.
+     */
     public static final ErrorType KDC_ERR_S_OLD_MAST_KVNO = new ErrorType( 5,
         "Server's key encrypted in old master key" );
+
+    /**
+     * Client not found in Kerberos database.
+     */
     public static final ErrorType KDC_ERR_C_PRINCIPAL_UNKNOWN = new ErrorType( 6,
         "Client not found in Kerberos database" );
+
+    /**
+     * Server not found in Kerberos database.
+     */
     public static final ErrorType KDC_ERR_S_PRINCIPAL_UNKNOWN = new ErrorType( 7,
         "Server not found in Kerberos database" );
+
+    /**
+     * Multiple principal entries in database.
+     */
     public static final ErrorType KDC_ERR_PRINCIPAL_NOT_UNIQUE = new ErrorType( 8,
         "Multiple principal entries in database" );
+
+    /**
+     * The client or server has a null key.
+     */
     public static final ErrorType KDC_ERR_NULL_KEY = new ErrorType( 9, "The client or server has a null key" );
+
+    /**
+     * Ticket not eligible for postdating.
+     */
     public static final ErrorType KDC_ERR_CANNOT_POSTDATE = new ErrorType( 10, "Ticket not eligible for postdating" );
+
+    /**
+     * Requested start time is later than end time.
+     */
     public static final ErrorType KDC_ERR_NEVER_VALID = new ErrorType( 11,
         "Requested start time is later than end time" );
+
+    /**
+     * KDC policy rejects request.
+     */
     public static final ErrorType KDC_ERR_POLICY = new ErrorType( 12, "KDC policy rejects request" );
+
+    /**
+     * KDC cannot accommodate requested option.
+     */
     public static final ErrorType KDC_ERR_BADOPTION = new ErrorType( 13, "KDC cannot accommodate requested option" );
+
+    /**
+     * KDC has no support for encryption type.
+     */
     public static final ErrorType KDC_ERR_ETYPE_NOSUPP = new ErrorType( 14, "KDC has no support for encryption type" );
+
+    /**
+     * KDC has no support for checksum type.
+     */
     public static final ErrorType KDC_ERR_SUMTYPE_NOSUPP = new ErrorType( 15, "KDC has no support for checksum type" );
+
+    /**
+     * KDC has no support for padata type.
+     */
     public static final ErrorType KDC_ERR_PADATA_TYPE_NOSUPP = new ErrorType( 16, "KDC has no support for padata type" );
+
+    /**
+     * KDC has no support for transitedEncoding type.
+     */
     public static final ErrorType KDC_ERR_TRTYPE_NOSUPP = new ErrorType( 17,
         "KDC has no support for transitedEncoding type" );
+
+    /**
+     * Clients credentials have been revoked.
+     */
     public static final ErrorType KDC_ERR_CLIENT_REVOKED = new ErrorType( 18, "Clients credentials have been revoked" );
+
+    /**
+     * Credentials for server have been revoked.
+     */
     public static final ErrorType KDC_ERR_SERVICE_REVOKED = new ErrorType( 19,
         "Credentials for server have been revoked" );
+
+    /**
+     * TGT has been revoked.
+     */
     public static final ErrorType KDC_ERR_TGT_REVOKED = new ErrorType( 20, "TGT has been revoked" );
+
+    /**
+     * Client not yet valid - try again later.
+     */
     public static final ErrorType KDC_ERR_CLIENT_NOTYET = new ErrorType( 21, "Client not yet valid - try again later" );
+
+    /**
+     * Server not yet valid - try again later.
+     */
     public static final ErrorType KDC_ERR_SERVICE_NOTYET = new ErrorType( 22, "Server not yet valid - try again later" );
+
+    /**
+     * Password has expired - change password to reset.
+     */
     public static final ErrorType KDC_ERR_KEY_EXPIRED = new ErrorType( 23,
         "Password has expired - change password to reset" );
+
+    /**
+     * Pre-authentication information was invalid.
+     */
     public static final ErrorType KDC_ERR_PREAUTH_FAILED = new ErrorType( 24,
         "Pre-authentication information was invalid" );
+
+    /**
+     * Additional pre-authentication required.
+     */
     public static final ErrorType KDC_ERR_PREAUTH_REQUIRED = new ErrorType( 25,
         "Additional pre-authentication required" );
+
+    /**
+     * Requested server and ticket don't match.
+     */
     public static final ErrorType KDC_ERR_SERVER_NOMATCH = new ErrorType( 26, "Requested server and ticket don't match" );
+
+    /**
+     * Server valid for user2user only.
+     */
     public static final ErrorType KDC_ERR_MUST_USE_USER2USER = new ErrorType( 27, "Server valid for user2user only" );
+
+    /**
+     * KDC Policy rejects transitedEncoding path.
+     */
     public static final ErrorType KDC_ERR_PATH_NOT_ACCEPTED = new ErrorType( 28,
         "KDC Policy rejects transitedEncoding path" );
+
+    /**
+     * A service is not available.
+     */
     public static final ErrorType KDC_ERR_SVC_UNAVAILABLE = new ErrorType( 29, "A service is not available" );
+
+    /**
+     * Integrity check on decrypted field failed.
+     */
     public static final ErrorType KRB_AP_ERR_BAD_INTEGRITY = new ErrorType( 31,
         "Integrity check on decrypted field failed" );
+
+    /**
+     * Ticket expired.
+     */
     public static final ErrorType KRB_AP_ERR_TKT_EXPIRED = new ErrorType( 32, "Ticket expired" );
+
+    /**
+     * Ticket not yet valid.
+     */
     public static final ErrorType KRB_AP_ERR_TKT_NYV = new ErrorType( 33, "Ticket not yet valid" );
+
+    /**
+     * Request is a replay.
+     */
     public static final ErrorType KRB_AP_ERR_REPEAT = new ErrorType( 34, "Request is a replay" );
+
+    /**
+     * The ticket isn't for us.
+     */
     public static final ErrorType KRB_AP_ERR_NOT_US = new ErrorType( 35, "The ticket isn't for us" );
+
+    /**
+     * Ticket and authenticator don't match.
+     */
     public static final ErrorType KRB_AP_ERR_BADMATCH = new ErrorType( 36, "Ticket and authenticator don't match" );
+
+    /**
+     * Clock skew too great.
+     */
     public static final ErrorType KRB_AP_ERR_SKEW = new ErrorType( 37, "Clock skew too great" );
+
+    /**
+     * Incorrect net address.
+     */
     public static final ErrorType KRB_AP_ERR_BADADDR = new ErrorType( 38, "Incorrect net address" );
+
+    /**
+     * Protocol version mismatch.
+     */
     public static final ErrorType KRB_AP_ERR_BADVERSION = new ErrorType( 39, "Protocol version mismatch" );
+
+    /**
+     * Invalid msg type.
+     */
     public static final ErrorType KRB_AP_ERR_MSG_TYPE = new ErrorType( 40, "Invalid msg type" );
+
+    /**
+     * Message stream modified.
+     */
     public static final ErrorType KRB_AP_ERR_MODIFIED = new ErrorType( 41, "Message stream modified" );
+
+    /**
+     * Message out of order.
+     */
     public static final ErrorType KRB_AP_ERR_BADORDER = new ErrorType( 42, "Message out of order" );
+
+    /**
+     * Specified version of key is not available.
+     */
     public static final ErrorType KRB_AP_ERR_BADKEYVER = new ErrorType( 44, "Specified version of key is not available" );
+
+    /**
+     * Service key not available.
+     */
     public static final ErrorType KRB_AP_ERR_NOKEY = new ErrorType( 45, "Service key not available" );
+
+    /**
+     * Mutual authentication failed.
+     */
     public static final ErrorType KRB_AP_ERR_MUT_FAIL = new ErrorType( 46, "Mutual authentication failed" );
+
+    /**
+     * Incorrect message direction.
+     */
     public static final ErrorType KRB_AP_ERR_BADDIRECTION = new ErrorType( 47, "Incorrect message direction" );
+
+    /**
+     * Alternative authentication method required.
+     */
     public static final ErrorType KRB_AP_ERR_METHOD = new ErrorType( 48, "Alternative authentication method required" );
+
+    /**
+     * Incorrect sequence number in message.
+     */
     public static final ErrorType KRB_AP_ERR_BADSEQ = new ErrorType( 49, "Incorrect sequence number in message" );
+
+    /**
+     * Inappropriate type of checksum in message.
+     */
     public static final ErrorType KRB_AP_ERR_INAPP_CKSUM = new ErrorType( 50,
         "Inappropriate type of checksum in message" );
+
+    /**
+     * Generic error (description in e-text).
+     */
     public static final ErrorType KRB_ERR_GENERIC = new ErrorType( 60, "Generic error (description in e-text)" );
+
+    /**
+     * Field is too long for this implementation.
+     */
     public static final ErrorType KRB_ERR_FIELD_TOOLONG = new ErrorType( 61,
         "Field is too long for this implementation" );
+
+    /**
+     * Client is not trusted.
+     */
     public static final ErrorType KRB_ERR_CLIENT_NOT_TRUSTED = new ErrorType( 62, "Client is not trusted" );
+
+    /**
+     * KDC is not trusted.
+     */
     public static final ErrorType KRB_ERR_KDC_NOT_TRUSTED = new ErrorType( 63, "KDC is not trusted" );
+
+    /**
+     * Signature is invalid.
+     */
     public static final ErrorType KRB_ERR_INVALID_SIG = new ErrorType( 64, "Signature is invalid" );
+
+    /**
+     * Key too weak.
+     */
     public static final ErrorType KRB_ERR_KEY_TOO_WEAK = new ErrorType( 65, "Key too weak" );
+
+    /**
+     * Certificates do not match.
+     */
     public static final ErrorType KRB_ERR_CERTIFICATE_MISMATCH = new ErrorType( 66, "Certificates do not match" );
+
+    /**
+     * No tgt for user-to-user authentication.
+     */
     public static final ErrorType KRB_AP_ERR_NO_TGT = new ErrorType( 67, "No tgt for user-to-user authentication" );
+
+    /**
+     * Wrong realm.
+     */
     public static final ErrorType KRB_ERR_WRONG_REALM = new ErrorType( 68, "Wrong realm" );
+
+    /**
+     * User-to-user authentication required.
+     */
     public static final ErrorType KRB_AP_ERR_USER_TO_USER_REQUIRED = new ErrorType( 69,
         "User-to-user authentication required" );
+
+    /**
+     * Can't verify certificate.
+     */
     public static final ErrorType KRB_ERR_CANT_VERIFY_CERTIFICATE = new ErrorType( 70, "Can't verify certificate" );
+
+    /**
+     * Invalid certificate.
+     */
     public static final ErrorType KRB_ERR_INVALID_CERTIFICATE = new ErrorType( 71, "Invalid certificate" );
+
+    /**
+     * Revoked certificate.
+     */
     public static final ErrorType KRB_ERR_REVOKED_CERTIFICATE = new ErrorType( 72, "Revoked certificate" );
+
+    /**
+     * Revocation status unknown.
+     */
     public static final ErrorType KRB_ERR_REVOCATION_STATUS_UNKNOWN = new ErrorType( 73, "Revocation status unknown" );
+
+    /**
+     * Revocation status unavailable.
+     */
     public static final ErrorType KRB_ERR_REVOCATION_STATUS_UNAVAILABLE = new ErrorType( 74,
         "Revocation status unavailable" );
+
+    /**
+     * Client names do not match.
+     */
     public static final ErrorType KRB_ERR_CLIENT_NAME_MISMATCH = new ErrorType( 75, "Client names do not match" );
+
+    /**
+     * KDC names do not match.
+     */
     public static final ErrorType KRB_ERR_KDC_NAME_MISMATCH = new ErrorType( 76, "KDC names do not match" );
 
-    /** Array for building a List of VALUES. */
+    /**
+     * Array for building a List of VALUES.
+     */
     private static final ErrorType[] values =
         { KDC_ERR_NONE, KDC_ERR_NAME_EXP, KDC_ERR_SERVICE_EXP, KDC_ERR_BAD_PVNO, KDC_ERR_C_OLD_MAST_KVNO,
             KDC_ERR_S_OLD_MAST_KVNO, KDC_ERR_C_PRINCIPAL_UNKNOWN, KDC_ERR_S_PRINCIPAL_UNKNOWN,
@@ -142,20 +403,26 @@ public final class ErrorType implements Comparable
             KRB_ERR_REVOCATION_STATUS_UNKNOWN, KRB_ERR_REVOCATION_STATUS_UNAVAILABLE, KRB_ERR_CLIENT_NAME_MISMATCH,
             KRB_ERR_KDC_NAME_MISMATCH };
 
-    /** a list of all the error type constants */
+    /**
+     * A List of all the error type constants.
+     */
     public static final List VALUES = Collections.unmodifiableList( Arrays.asList( values ) );
 
-    /** the name of the error type */
+    /**
+     * The name of the error type.
+     */
     private final String name;
 
-    /** the value/code for the error type */
+    /**
+     * The value/code for the error type.
+     */
     private final int ordinal;
 
 
     /**
      * Private constructor prevents construction outside of this class.
      */
-    private ErrorType(int ordinal, String name)
+    private ErrorType( int ordinal, String name )
     {
         this.ordinal = ordinal;
         this.name = name;
