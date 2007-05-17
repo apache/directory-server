cd9f0fbd35cfc4639009cb6cab109e73eec79610
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-17 06:30:43 +0000

    Javadocs and minor warning clean-up.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/kerberos-encryption-types@538815 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/messages/MessageType.java
+++ kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/messages/MessageType.java
@@ -32,61 +32,110 @@ import java.util.List;
 public final class MessageType implements Comparable
 {
     /**
-     * Enumeration elements are constructed once upon class loading.
-     * Order of appearance here determines the order of compareTo.
+     * Constant for the "null" message type.
      */
     public static final MessageType NULL = new MessageType( 0, "null" );
+
+    /**
+     * Constant for the "initial authentication request" message type.
+     */
     public static final MessageType KRB_AS_REQ = new MessageType( 10, "initial authentication request" );
+
+    /**
+     * Constant for the "initial authentication response" message type.
+     */
     public static final MessageType KRB_AS_REP = new MessageType( 11, "initial authentication response" );
+
+    /**
+     * Constant for the "request for authentication based on TGT" message type.
+     */
     public static final MessageType KRB_TGS_REQ = new MessageType( 12, "request for authentication based on TGT" );
+
+    /**
+     * Constant for the "response to authentication based on TGT" message type.
+     */
     public static final MessageType KRB_TGS_REP = new MessageType( 13, "response to authentication based on TGT" );
+
+    /**
+     * Constant for the "application request" message type.
+     */
     public static final MessageType KRB_AP_REQ = new MessageType( 14, "application request" );
+
+    /**
+     * Constant for the "application response" message type.
+     */
     public static final MessageType KRB_AP_REP = new MessageType( 15, "application response" );
+
+    /**
+     * Constant for the "safe (checksummed) application message" message type.
+     */
     public static final MessageType KRB_SAFE = new MessageType( 20, "safe (checksummed) application message" );
+
+    /**
+     * Constant for the "private (encrypted) application message" message type.
+     */
     public static final MessageType KRB_PRIV = new MessageType( 21, "private (encrypted) application message" );
+
+    /**
+     * Constant for the "private (encrypted) message to forward credentials" message type.
+     */
     public static final MessageType KRB_CRED = new MessageType( 22,
         "private (encrypted) message to forward credentials" );
+
+    /**
+     * Constant for the "encrypted application reply part" message type.
+     */
     public static final MessageType ENC_AP_REP_PART = new MessageType( 27, "encrypted application reply part" );
+
+    /**
+     * Constant for the "encrypted private message part" message type.
+     */
     public static final MessageType ENC_PRIV_PART = new MessageType( 28, "encrypted private message part" );
+
+    /**
+     * Constant for the "error response" message type.
+     */
     public static final MessageType KRB_ERROR = new MessageType( 30, "error response" );
 
-    /** Array for building a List of VALUES. */
+    /**
+     * Array for building a List of VALUES.
+     */
     private static final MessageType[] values =
         { NULL, KRB_AS_REQ, KRB_AS_REP, KRB_TGS_REQ, KRB_TGS_REP, KRB_AP_REQ, KRB_AP_REP, KRB_SAFE, KRB_PRIV, KRB_CRED,
             ENC_AP_REP_PART, ENC_PRIV_PART, KRB_ERROR };
 
-    /** A list of all the message type constants. */
+    /**
+     * A list of all the message type constants.
+     */
     public static final List VALUES = Collections.unmodifiableList( Arrays.asList( values ) );
 
-    /** the name of the message type */
+    /**
+     * The name of the message type.
+     */
     private final String name;
 
-    /** the value/code for the message type */
+    /**
+     * The value/code for the message type.
+     */
     private final int ordinal;
 
 
     /**
      * Private constructor prevents construction outside of this class.
      */
-    private MessageType(int ordinal, String name)
+    private MessageType( int ordinal, String name )
     {
         this.ordinal = ordinal;
         this.name = name;
     }
 
 
-    public String toString()
-    {
-        return name + " (" + ordinal + ")";
-    }
-
-
-    public int compareTo( Object that )
-    {
-        return ordinal - ( ( MessageType ) that ).ordinal;
-    }
-
-
+    /**
+     * Returns the message type when specified by its ordinal.
+     *
+     * @param type
+     * @return The message type.
+     */
     public static MessageType getTypeByOrdinal( int type )
     {
         for ( int ii = 0; ii < values.length; ii++ )
@@ -101,8 +150,25 @@ public final class MessageType implements Comparable
     }
 
 
+    /**
+     * Returns the number associated with this message type.
+     *
+     * @return The message type ordinal.
+     */
     public int getOrdinal()
     {
         return ordinal;
+    }
+
+
+    public int compareTo( Object that )
+    {
+        return ordinal - ( ( MessageType ) that ).ordinal;
+    }
+
+
+    public String toString()
+    {
+        return name + " (" + ordinal + ")";
     }
 }
