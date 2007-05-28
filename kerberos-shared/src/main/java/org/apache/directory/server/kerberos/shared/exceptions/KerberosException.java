0575aef839541862834773d4159476cb464b7fb1
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-28 22:24:16 +0000

    Clean-up of KerberosException to better support nested exceptions.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@542350 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/exceptions/KerberosException.java
+++ kerberos-shared/src/main/java/org/apache/directory/server/kerberos/shared/exceptions/KerberosException.java
@@ -31,7 +31,7 @@ public class KerberosException extends Exception
     private static final long serialVersionUID = 2968072183596955597L;
 
     /**
-     * The Kerberos error code associated with this exception
+     * The Kerberos error code associated with this exception.
      */
     private final int errorCode;
 
@@ -42,17 +42,126 @@ public class KerberosException extends Exception
     private byte[] explanatoryData;
 
 
-    // ------------------------------------------------------------------------
-    // C O N S T R U C T O R S
-    // ------------------------------------------------------------------------
+    /**
+     * Creates a KerberosException with an {@link ErrorType}.
+     *
+     * @param errorType The error type associated with this KerberosException.
+     */
+    public KerberosException( ErrorType errorType )
+    {
+        super( errorType.getMessage() );
+
+        this.errorCode = errorType.getOrdinal();
+    }
+
+
+    /**
+     * Creates a KerberosException with an {@link ErrorType} and an
+     * underlying {@link Throwable} that caused this fault.
+     *
+     * @param errorType The error type associated with this KerberosException.
+     * @param cause The underlying failure, if any.
+     */
+    public KerberosException( ErrorType errorType, Throwable cause )
+    {
+        super( errorType.getMessage(), cause );
+
+        this.errorCode = errorType.getOrdinal();
+    }
+
+
+    /**
+     * Creates a KerberosException with an {@link ErrorType} and a custom error message.
+     *
+     * @param errorType The {@link ErrorType} associated with this KerberosException.
+     * @param msg A custom error message for this KerberosException.
+     */
+    public KerberosException( ErrorType errorType, String msg )
+    {
+        super( msg );
+
+        this.errorCode = errorType.getOrdinal();
+    }
+
+
+    /**
+     * Creates a KerberosException with an {@link ErrorType}, a custom error message, and an
+     * underlying {@link Throwable} that caused this fault.
+     *
+     * @param errorType The error type associated with this KerberosException.
+     * @param msg A custom error message for this KerberosException.
+     * @param cause The underlying failure, if any.
+     */
+    public KerberosException( ErrorType errorType, String msg, Throwable cause )
+    {
+        super( msg, cause );
+
+        this.errorCode = errorType.getOrdinal();
+    }
+
+
+    /**
+     * Creates a KerberosException with an {@link ErrorType} and data helping to
+     * explain what caused this fault.
+     *
+     * @param errorType The error type associated with this KerberosException.
+     * @param explanatoryData Data helping to explain this fault, if any.
+     */
+    public KerberosException( ErrorType errorType, byte[] explanatoryData )
+    {
+        super( errorType.getMessage() );
+
+        this.errorCode = errorType.getOrdinal();
+        this.explanatoryData = explanatoryData;
+    }
+
+
+    /**
+     * Creates a KerberosException with an {@link ErrorType}, data helping to
+     * explain what caused this fault, and an underlying {@link Throwable} that caused this fault.
+     *
+     * @param errorType The error type associated with this KerberosException.
+     * @param explanatoryData Data helping to explain this fault, if any.
+     * @param cause The underlying failure, if any.
+     */
+    public KerberosException( ErrorType errorType, byte[] explanatoryData, Throwable cause )
+    {
+        super( errorType.getMessage(), cause );
+
+        this.errorCode = errorType.getOrdinal();
+        this.explanatoryData = explanatoryData;
+    }
+
+
+    /**
+     * Gets the protocol error code associated with this KerberosException.
+     *
+     * @return The error code associated with this KerberosException.
+     */
+    public int getErrorCode()
+    {
+        return this.errorCode;
+    }
+
+
+    /**
+     * Gets the explanatory data associated with this KerberosException.
+     *
+     * @return The explanatory data associated with this KerberosException.
+     */
+    public byte[] getExplanatoryData()
+    {
+        return explanatoryData;
+    }
+
 
     /**
      * Creates a KerberosException with an error code and a message.
      *
-     * @param errorCode the error code associated with this KerberosException
-     * @param msg the standard Kerberos error message for this KerberosException
+     * @param errorCode The error code associated with this KerberosException.
+     * @param msg The standard Kerberos error message for this KerberosException.
      */
-    public KerberosException(int errorCode, String msg)
+    protected KerberosException( int errorCode, String msg )
     {
         super( msg );
 
@@ -62,13 +171,13 @@ public class KerberosException extends Exception
 
     /**
      * Creates a KerberosException with an error code, a message and an
-     * underlying throwable that caused this fault.
+     * underlying {@link Throwable} that caused this fault.
      *
-     * @param errorCode the error code associated with this KerberosException
-     * @param msg the standard Kerberos error message for this KerberosException
-     * @param cause the underlying failure, if any
+     * @param errorCode The error code associated with this KerberosException.
+     * @param msg The standard Kerberos error message for this KerberosException.
+     * @param cause The underlying failure, if any.
      */
-    public KerberosException(int errorCode, String msg, Throwable cause)
+    protected KerberosException( int errorCode, String msg, Throwable cause )
     {
         super( msg, cause );
 
@@ -80,11 +189,11 @@ public class KerberosException extends Exception
      * Creates a KerberosException with an error code, a message, and data
      * helping to explain what caused this fault.
      *
-     * @param errorCode the error code associated with this KerberosException
-     * @param msg the standard Kerberos error message for this KerberosException
-     * @param explanatoryData data helping to explain this fault, if any
+     * @param errorCode The error code associated with this KerberosException.
+     * @param msg The standard Kerberos error message for this KerberosException.
+     * @param explanatoryData Data helping to explain this fault, if any.
      */
-    public KerberosException(int errorCode, String msg, byte[] explanatoryData)
+    protected KerberosException( int errorCode, String msg, byte[] explanatoryData )
     {
         super( msg );
 
@@ -94,66 +203,19 @@ public class KerberosException extends Exception
 
 
     /**
-     * Creates a KerberosException with an error type.
+     * Creates a KerberosException with an error code, a message, and data
+     * helping to explain what caused this fault.
      *
-     * @param errorType the error type associated with this KerberosException
+     * @param errorCode The error code associated with this KerberosException.
+     * @param msg The standard Kerberos error message for this KerberosException.
+     * @param explanatoryData Data helping to explain this fault, if any.
+     * @param cause The underlying failure, if any.
      */
-    public KerberosException(ErrorType errorType)
+    protected KerberosException( int errorCode, String msg, byte[] explanatoryData, Throwable cause )
     {
-        super( errorType.getMessage() );
+        super( msg, cause );
 
-        this.errorCode = errorType.getOrdinal();
-    }
-
-
-    /**
-     * Creates a KerberosException with an error type and a custom error message.
-     *
-     * @param errorType the error type associated with this KerberosException
-     * @param msg a custom error message for this KerberosException
-     */
-    public KerberosException(ErrorType errorType, String msg)
-    {
-        super( msg );
-
-        this.errorCode = errorType.getOrdinal();
-    }
-
-
-    /**
-     * Creates a KerberosException with an error type and data helping to
-     * explain what caused this fault.
-     *
-     * @param errorType the error type associated with this KerberosException
-     * @param explanatoryData data helping to explain this fault, if any
-     */
-    public KerberosException(ErrorType errorType, byte[] explanatoryData)
-    {
-        super( errorType.getMessage() );
-
-        this.errorCode = errorType.getOrdinal();
+        this.errorCode = errorCode;
         this.explanatoryData = explanatoryData;
-    }
-
-
-    /**
-     * Gets the protocol error code associated with this KerberosException.
-     *
-     * @return the error code associated with this KerberosException
-     */
-    public int getErrorCode()
-    {
-        return this.errorCode;
-    }
-
-
-    /**
-     * Gets the explanatory data associated with this KerberosException.
-     *
-     * @return the explanatory data associated with this KerberosException
-     */
-    public byte[] getExplanatoryData()
-    {
-        return explanatoryData;
     }
 }
