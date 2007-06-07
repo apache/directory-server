42ece7b02d9b79bbba8a4a86eed996ca7557f22a
Christine Koppelt <ckoppelt@apache.org>
2007-06-07 20:57:02 +0000

    added simple batch script for starting ApacheDS as non-windows-service
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@545308 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server-main/apacheds.bat
@@ -0,0 +1,15 @@
+REM this script starts ApacheDS (as non-windows-service)
+
+@echo off
+
+IF NOT exist target/apacheds-server-main-1.5.1-SNAPSHOT-app.jar GOTO :MVN
+   echo uber jar exists
+   GOTO :JAVA
+
+:MVN
+   echo uber jar not found need to build it
+   mvn clean assembly:assembly
+   GOTO :JAVA
+
+:JAVA
+   java -Dlog4j.configuration=file:///%cd%/log4j.properties -jar target/apacheds-server-main-1.5.1-SNAPSHOT-app.jar server.xml
