1da86dba08a3e722f438a69d8c78c196dfeee86f
Alex Karasulu <akarasulu@apache.org>
2007-08-23 15:58:05 +0000

    use StringTools.EMPTY_BYTES
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@569052 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/message/CascadeControl.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/message/CascadeControl.java
@@ -19,6 +19,8 @@
  */
 package org.apache.directory.shared.ldap.message;
 
+import org.apache.directory.shared.ldap.util.StringTools;
+
 
 /**
  * The control for peforming a cascade of operations like delete and modify.
@@ -29,9 +31,8 @@ package org.apache.directory.shared.ldap.message;
 public class CascadeControl extends ControlImpl
 {
     private static final long serialVersionUID = -2356861450876343999L;
-    private static final byte[] EMPTY_ARRAY = new byte[0];
-    
     public static final String CONTROL_OID = "1.3.6.1.4.1.18060.0.0.1";
+
 
     public CascadeControl()
     {
@@ -42,6 +43,6 @@ public class CascadeControl extends ControlImpl
 
     public byte[] getEncodedValue()
     {
-        return EMPTY_ARRAY;
+        return StringTools.EMPTY_BYTES;
     }
 }
