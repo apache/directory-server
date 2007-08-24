279e5291353d645a0e7c88307479cf37a9d0e123
Emmanuel Lecharny <elecharny@apache.org>
2007-08-24 23:26:35 +0000

    Added a return if we get some exception, instead of considering both DN are equals
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@569570 13f79535-47bb-0310-9956-ffa450edef68

--- schema-registries/src/main/java/org/apache/directory/server/schema/DnComparator.java
+++ schema-registries/src/main/java/org/apache/directory/server/schema/DnComparator.java
@@ -72,6 +72,7 @@ public class DnComparator implements Comparator
         catch ( NamingException e )
         {
             // -- what do we do here ?
+        	return -1;
         }
         
         return dn0.compareTo( dn1 );
