9cce62d150f068ca281c2d42a6a68f815687a5bd
Alex Karasulu <akarasulu@apache.org>
2007-09-01 04:09:41 +0000

    moving out server-main into installers area since this will be the basis to the simple non-installer based noarch tarball/zip deliverable
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@571681 13f79535-47bb-0310-9956-ffa450edef68

--- server-main/apacheds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e target/apacheds-server-main-1.5.1-SNAPSHOT-app.jar ] ; then
-  echo uber jar exists
-else
-  echo uber jar not found need to build it
-  mvn clean assembly:assembly
-fi
-
-java -Dlog4j.debug -Dlog4j.configuration=file:./log4j.properties -jar target/apacheds-server-main-1.5.1-SNAPSHOT-app.jar server.xml 
