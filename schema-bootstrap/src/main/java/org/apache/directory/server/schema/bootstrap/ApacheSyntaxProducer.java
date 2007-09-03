80faeb3deac126dadf144daaa9d7063bab9681cc
Emmanuel Lecharny <elecharny@apache.org>
2007-09-03 00:10:42 +0000

    Removed useless imports
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@572204 13f79535-47bb-0310-9956-ffa450edef68

--- schema-bootstrap/src/main/java/org/apache/directory/server/schema/bootstrap/ApacheSyntaxProducer.java
+++ schema-bootstrap/src/main/java/org/apache/directory/server/schema/bootstrap/ApacheSyntaxProducer.java
@@ -21,10 +21,10 @@ package org.apache.directory.server.schema.bootstrap;
 
 
 import javax.naming.NamingException;
-import org.apache.directory.server.schema.registries.*;
+
+import org.apache.directory.server.schema.registries.Registries;
 import org.apache.directory.shared.ldap.constants.SchemaConstants;
 import org.apache.directory.shared.ldap.schema.AbstractSyntax;
-import org.apache.directory.shared.ldap.schema.Syntax;
 import org.apache.directory.shared.ldap.schema.syntax.JavaByteSyntaxChecker;
 import org.apache.directory.shared.ldap.schema.syntax.JavaIntegerSyntaxChecker;
 import org.apache.directory.shared.ldap.schema.syntax.JavaLongSyntaxChecker;
