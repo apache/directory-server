e869a6e6f82dcb9a87127e3caf5a81c8ef18ec36
Emmanuel Lecharny <elecharny@apache.org>
2007-09-14 20:18:35 +0000

    Forgot to add the EqualityNode class
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@575784 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap/src/main/java/org/apache/directory/shared/ldap/filter/EqualityNode.java
@@ -0,0 +1,99 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.shared.ldap.filter;
+
+
+/**
+ * A assertion value node for Equality.
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Revision: 519266 $
+ */
+public class EqualityNode extends SimpleNode
+{
+    /**
+     * Creates a new Equality object.
+     * 
+     * @param attribute the attribute name
+     * @param value the value to test for
+     */
+    public EqualityNode( String attribute, byte[] value )
+    {
+        super( attribute, value );
+    }
+
+
+    /**
+     * Creates a new Equality object.
+     * 
+     * @param attribute the attribute name
+     * @param value the value to test for
+     */
+    public EqualityNode( String attribute, String value )
+    {
+        super( attribute, value );
+    }
+
+    /**
+     * @see org.apache.directory.shared.ldap.filter.ExprNode#printToBuffer(
+     *      java.lang.StringBuilder)
+     */
+    public StringBuilder printToBuffer( StringBuilder buf )
+    {
+        buf.append( '(' ).append( getAttribute() ).append( "=" ).append( value ).append( ')' );
+    	return super.printToBuffer( buf );
+    }
+
+    
+    /**
+     * @see org.apache.directory.shared.ldap.filter.ExprNode#accept(
+     *      org.apache.directory.shared.ldap.filter.FilterVisitor)
+     */
+    public void accept( FilterVisitor visitor )
+    {
+        if ( visitor.canVisit( this ) )
+        {
+            visitor.visit( this );
+        }
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals( Object other )
+    {
+        if ( this == other )
+        {
+            return true;
+        }
+
+        if ( !( other instanceof EqualityNode ) )
+        {
+            return false;
+        }
+
+        EqualityNode otherNode = (EqualityNode) other; 
+        
+        return ( value == null ? otherNode.value == null : value.equals( otherNode.value ) );
+    }
+}
