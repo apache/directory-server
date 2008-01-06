aaa561e353e70bf52cb9e30b28755f0037ace40f
Emmanuel Lecharny <elecharny@apache.org>
2008-01-06 23:05:12 +0000

    forgot to add this file
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/bigbang@609462 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap/src/main/java/org/apache/directory/shared/ldap/entry/client/ClientAttribute.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.directory.shared.ldap.entry.client;
+
+
+import org.apache.directory.shared.ldap.schema.AttributeType;
+import org.apache.directory.shared.ldap.entry.EntryAttribute;
+
+import javax.naming.NamingException;
+
+
+/**
+ * The server specific interface extending the EntryAttribute interface. It adds
+ * three more methods which are Server side.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+public interface ClientAttribute extends EntryAttribute<ClientValue<?>>, Iterable<ClientValue<?>>, Cloneable
+{
+    /**
+     * Get's the user provided identifier for this entry.  This is the value
+     * that will be used as the identifier for the attribute within the
+     * entry.  If this is a commonName attribute for example and the user
+     * provides "COMMONname" instead when adding the entry then this is
+     * the format the user will have that entry returned by the directory
+     * server.  To do so we store this value as it was given and track it
+     * in the attribute using this property.
+     *
+     * @return the user provided identifier for this attribute
+     */
+    String getUpId();
+
+    
+    /**
+     * Checks to see if this attribute is valid along with the values it contains.
+     *
+     * @return true if the attribute and it's values are valid, false otherwise
+     * @throws NamingException if there is a failure to check syntaxes of values
+     */
+    boolean isValid() throws NamingException;
+}
