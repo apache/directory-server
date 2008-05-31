62b5b3e74fe585a8744e28236f20ce67b36bdf55
Alex Karasulu <akarasulu@apache.org>
2008-05-31 05:34:27 +0000

    imports
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/bigbang@661950 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/directory/server/core/CoreSession.java
+++ core/src/main/java/org/apache/directory/server/core/CoreSession.java
@@ -27,8 +27,6 @@ import java.util.Set;
 import javax.naming.ldap.Control;
 
 import org.apache.directory.server.core.authn.LdapPrincipal;
-import org.apache.directory.server.core.cursor.Cursor;
-import org.apache.directory.server.core.entry.ClonedServerEntry;
 import org.apache.directory.server.core.entry.ServerEntry;
 import org.apache.directory.server.core.filtering.EntryFilteringCursor;
 import org.apache.directory.server.core.interceptor.context.OperationContext;
