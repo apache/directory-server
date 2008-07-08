05e126b7cc12a0e5719b8507ea0038e20cea4e13
Emmanuel Lecharny <elecharny@apache.org>
2008-07-08 12:09:52 +0000

    Added a missing header
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/bigbang@674789 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/filter/FilterParser.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/filter/FilterParser.java
@@ -28,6 +28,12 @@ import org.apache.directory.shared.ldap.util.Position;
 import org.apache.directory.shared.ldap.util.StringTools;
 
 
+/**
+ * This class parse a Ldap filter. The grammar is given in RFC 4515
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
 public class FilterParser
 {
     /**
