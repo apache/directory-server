a89c16db82504208edc96c228eabb0d94bc6d5ff
Alex Karasulu <akarasulu@apache.org>
2008-08-04 07:46:39 +0000

    adding code and tests to handle referrals in compare operations
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/bigbang@682279 13f79535-47bb-0310-9956-ffa450edef68

--- protocol-newldap/src/main/java/org/apache/directory/server/newldap/handlers/NewCompareHandler.java
+++ protocol-newldap/src/main/java/org/apache/directory/server/newldap/handlers/NewCompareHandler.java
@@ -20,13 +20,20 @@
 package org.apache.directory.server.newldap.handlers;
 
 
+import javax.naming.InvalidNameException;
+import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
-import javax.naming.ReferralException;
 
+import org.apache.directory.server.core.entry.ClonedServerEntry;
 import org.apache.directory.server.newldap.LdapSession;
+import org.apache.directory.shared.ldap.NotImplementedException;
+import org.apache.directory.shared.ldap.constants.SchemaConstants;
+import org.apache.directory.shared.ldap.entry.EntryAttribute;
+import org.apache.directory.shared.ldap.entry.Value;
 import org.apache.directory.shared.ldap.exception.LdapException;
 import org.apache.directory.shared.ldap.message.CompareRequest;
 import org.apache.directory.shared.ldap.message.LdapResult;
+import org.apache.directory.shared.ldap.message.ManageDsaITControl;
 import org.apache.directory.shared.ldap.message.ReferralImpl;
 import org.apache.directory.shared.ldap.message.ResultCodeEnum;
 import org.apache.directory.shared.ldap.name.LdapDN;
@@ -46,12 +53,200 @@ public class NewCompareHandler extends LdapRequestHandler<CompareRequest>
 {
     private static final Logger LOG = LoggerFactory.getLogger( NewCompareHandler.class );
     
-
     /** Speedup for logs */
     private static final boolean IS_DEBUG = LOG.isDebugEnabled();
 
     
+    /**
+     * Main entry point for handler.
+     * 
+     * @see LdapRequestHandler#handle(LdapSession, org.apache.directory.shared.ldap.message.Request)
+     */
     public void handle( LdapSession session, CompareRequest req ) throws Exception
+    {
+        LOG.debug( "Handling compare request: {}", req );
+        
+        if ( req.getControls().containsKey( ManageDsaITControl.CONTROL_OID ) )
+        {
+            LOG.debug( "ManageDsaITControl detected." );
+            handleIgnoringReferrals( session, req );
+        }
+        else
+        {
+            LOG.debug( "ManageDsaITControl NOT detected." );
+            handleWithReferrals( session, req );
+        }
+    }
+    
+    
+    /**
+     * Handles processing with referrals without ManageDsaIT control.
+     */
+    private void handleWithReferrals( LdapSession session, CompareRequest req )
+    {
+        LdapResult result = req.getResultResponse().getLdapResult();
+        ClonedServerEntry entry = null;
+
+        // -------------------------------------------------------------------
+        // Lookup Entry
+        // -------------------------------------------------------------------
+        
+        // try to lookup the entry but ignore exceptions when it does not   
+        // exist since entry may not exist but may have an ancestor that is a 
+        // referral - would rather attempt a lookup that fails then do check 
+        // for existence than have to do another lookup to get entry info
+
+        try
+        {
+            entry = session.getCoreSession().lookup( req.getName() );
+        }
+        catch ( NameNotFoundException e )
+        {
+            /* ignore */
+        }
+        catch ( Exception e )
+        {
+            /* serious and needs handling */
+            handleException( session, req, e );
+            return;
+        }
+        
+        // -------------------------------------------------------------------
+        // Handle Existing Entry
+        // -------------------------------------------------------------------
+        
+        if ( entry != null )
+        {
+            try
+            {
+                if ( entry.getOriginalEntry().contains( SchemaConstants.OBJECT_CLASS_AT, SchemaConstants.REFERRAL_OC ) )
+                {
+                    // -------------------------------------------------------
+                    // Entry IS Referral
+                    // -------------------------------------------------------
+         
+                    LOG.debug( "Entry is a referral: {}", entry );
+                    
+                    ReferralImpl refs = new ReferralImpl();
+                    result.setReferral( refs );
+                    result.setResultCode( ResultCodeEnum.REFERRAL );
+                    result.setErrorMessage( "Encountered referral attempting to handle compare request." );
+                    result.setMatchedDn( req.getName() );
+
+                    EntryAttribute refAttr = entry.getOriginalEntry().get( SchemaConstants.REF_AT );
+                    for ( Value<?> refval : refAttr )
+                    {
+                        refs.addLdapUrl( ( String ) refval.get() );
+                    }
+
+                    session.getIoSession().write( req.getResultResponse() );
+                    return;
+                }
+                else
+                {
+                    LOG.debug( "Entry is NOT a referral: {}", entry );
+                    handleIgnoringReferrals( session, req );
+                    return;
+                }
+            }
+            catch ( Exception e )
+            {
+                handleException( session, req, e );
+            }
+        }
+
+        // -------------------------------------------------------------------
+        // Handle Non-existing Entry
+        // -------------------------------------------------------------------
+        
+        // if the entry is null we still have to check for a referral ancestor
+        // also the referrals need to be adjusted based on the ancestor's ref
+        // values to yield the correct path to the entry to be compared in the
+        // target DSAs
+        
+        if ( entry == null )
+        {
+            ClonedServerEntry referralAncestor = null;
+            LdapDN lastMatchedDn = null;
+            LdapDN dn = ( LdapDN ) req.getName().clone();
+            
+            while ( ! dn.isEmpty() )
+            {
+                try
+                {
+                    referralAncestor = session.getCoreSession().lookup( dn );
+                    EntryAttribute oc = referralAncestor.getOriginalEntry().get( SchemaConstants.OBJECT_CLASS_AT );
+                    
+                    // make sure referral ancestor is really a referral if not
+                    // then we null it out so the proper handling can occur 
+                    // down below - the reason for this is because we want the
+                    // farthest ancestor, not the closest one.
+                    
+                    if ( ! oc.contains( SchemaConstants.REFERRAL_OC ) )
+                    {
+                        referralAncestor = null;
+                    }
+
+                    // set last matched DN only if not set yet because we want 
+                    // the closest ancestor for this value regardless of what 
+                    // kind of entry (referral or normal) it is.
+                    
+                    if ( lastMatchedDn == null )
+                    {
+                        lastMatchedDn = ( LdapDN ) dn.clone();
+                    }
+                }
+                catch ( NameNotFoundException e )
+                {
+                    // update the DN as we strip last component 
+                    try
+                    {
+                        dn = ( LdapDN ) dn.remove( dn.size() - 1 );
+                    }
+                    catch ( InvalidNameException e1 )
+                    {
+                        // never happens
+                    }
+                }
+                catch ( Exception e )
+                {
+                    handleException( session, req, e );
+                }
+            }
+
+            if ( referralAncestor == null )
+            {
+                result.setMatchedDn( lastMatchedDn );
+                result.setErrorMessage( "Entry not found." );
+                result.setResultCode( ResultCodeEnum.NO_SUCH_OBJECT );
+                session.getIoSession().write( req.getResultResponse() );
+                return;
+            }
+              
+            // if we get here then we have a valid referral ancestor
+            handleReferralOnAncestor( session, req, referralAncestor );
+        }
+    }
+
+    
+    /**
+     * Handles processing with referrals without ManageDsaIT control and with 
+     * an ancestor that is a referral.  The original entry was not found and 
+     * the walk of the ancestry returned a referral.
+     * 
+     * @param referralAncestor the farthest referral ancestor of the missing 
+     * entry  
+     */
+    public void handleReferralOnAncestor( LdapSession session, CompareRequest req, ClonedServerEntry referralAncestor )
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+    /**
+     * Handles processing without referrals with the ManageDsaIT control.
+     */
+    public void handleIgnoringReferrals( LdapSession session, CompareRequest req )
     {
         LdapResult result = req.getResultResponse().getLdapResult();
 
@@ -69,58 +264,54 @@ public class NewCompareHandler extends LdapRequestHandler<CompareRequest>
             result.setMatchedDn( req.getName() );
             session.getIoSession().write( req.getResultResponse() );
         }
-        catch ( ReferralException e )
-        {
-            ReferralImpl refs = new ReferralImpl();
-            result.setReferral( refs );
-            result.setResultCode( ResultCodeEnum.REFERRAL );
-            result.setErrorMessage( "Encountered referral attempting to handle compare request." );
-
-            result.setMatchedDn( (LdapDN)e.getResolvedName() );
-
-            do
-            {
-                refs.addLdapUrl( ( String ) e.getReferralInfo() );
-            }
-            while ( e.skipReferral() );
-            session.getIoSession().write( req.getResultResponse() );
-        }
         catch ( Exception e )
         {
-            String msg = "failed to compare entry " + req.getName() + ": " + e.getMessage();
-
-            if ( IS_DEBUG )
-            {
-                msg += ":\n" + ExceptionUtils.getStackTrace( e );
-            }
-
-            ResultCodeEnum code;
-
-            if ( e instanceof LdapException )
-            {
-                code = ( ( LdapException ) e ).getResultCode();
-            }
-            else
-            {
-                code = ResultCodeEnum.getBestEstimate( e, req.getType() );
-            }
-
-            result.setResultCode( code );
-            result.setErrorMessage( msg );
-
-            if ( e instanceof NamingException )
-            {
-                NamingException ne = ( NamingException ) e;
-
-                if ( ( ne.getResolvedName() != null )
-                    && ( ( code == ResultCodeEnum.NO_SUCH_OBJECT ) || ( code == ResultCodeEnum.ALIAS_PROBLEM )
-                        || ( code == ResultCodeEnum.INVALID_DN_SYNTAX ) || ( code == ResultCodeEnum.ALIAS_DEREFERENCING_PROBLEM ) ) )
-                {
-                    result.setMatchedDn( (LdapDN)ne.getResolvedName() );
-                }
-            }
-
-            session.getIoSession().write( req.getResultResponse() );
+            handleException( session, req, e );
         }
+    }
+    
+    
+    /**
+     * Handles processing with referrals without ManageDsaIT control.
+     */
+    private void handleException( LdapSession session, CompareRequest req, Exception e )
+    {
+        LdapResult result = req.getResultResponse().getLdapResult();
+
+        String msg = "failed to compare entry " + req.getName() + ": " + e.getMessage();
+        LOG.error( msg, e );
+
+        if ( IS_DEBUG )
+        {
+            msg += ":\n" + ExceptionUtils.getStackTrace( e );
+        }
+
+        ResultCodeEnum code;
+
+        if ( e instanceof LdapException )
+        {
+            code = ( ( LdapException ) e ).getResultCode();
+        }
+        else
+        {
+            code = ResultCodeEnum.getBestEstimate( e, req.getType() );
+        }
+
+        result.setResultCode( code );
+        result.setErrorMessage( msg );
+
+        if ( e instanceof NamingException )
+        {
+            NamingException ne = ( NamingException ) e;
+
+            if ( ( ne.getResolvedName() != null )
+                && ( ( code == ResultCodeEnum.NO_SUCH_OBJECT ) || ( code == ResultCodeEnum.ALIAS_PROBLEM )
+                    || ( code == ResultCodeEnum.INVALID_DN_SYNTAX ) || ( code == ResultCodeEnum.ALIAS_DEREFERENCING_PROBLEM ) ) )
+            {
+                result.setMatchedDn( (LdapDN)ne.getResolvedName() );
+            }
+        }
+
+        session.getIoSession().write( req.getResultResponse() );
     }
 }
--- server-integ/src/main/java/org/apache/directory/server/integ/ServerIntegrationUtils.java
+++ server-integ/src/main/java/org/apache/directory/server/integ/ServerIntegrationUtils.java
@@ -26,6 +26,8 @@ import javax.naming.ldap.Control;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
 
+import netscape.ldap.LDAPConnection;
+
 import org.apache.directory.server.constants.ServerDNConstants;
 import org.apache.directory.server.core.integ.IntegrationUtils;
 import org.apache.directory.server.newldap.LdapServer;
@@ -38,6 +40,12 @@ public class ServerIntegrationUtils extends IntegrationUtils
     /** The class logger */
     private static final Logger LOG = LoggerFactory.getLogger( ServerIntegrationUtils.class );
     private static final String CTX_FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
+
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 10389;
+    private static final String DEFAULT_ADMIN = ServerDNConstants.ADMIN_SYSTEM_DN;
+    private static final String DEFAULT_PASSWORD = "secret";
+
 
 
     /**
@@ -74,5 +82,89 @@ public class ServerIntegrationUtils extends IntegrationUtils
         env.put( Context.SECURITY_CREDENTIALS, "secret" );
         env.put( Context.SECURITY_AUTHENTICATION, "simple" );
         return new InitialLdapContext( env, controls );
+    }
+
+    
+    /**
+     * Creates a JNDI LdapContext with a connection over the wire using the 
+     * SUN LDAP provider.  The connection is made using the administrative 
+     * user as the principalDN.  The context is to the rootDSE.
+     *
+     * @param ldapServer the LDAP server to get the connection to
+     * @return an LdapContext as the administrative user to the RootDSE
+     * @throws Exception if there are problems creating the context
+     */
+    public static LdapContext getWiredContextThrowOnRefferal( LdapServer ldapServer ) throws Exception
+    {
+        LOG.debug( "Creating a wired context to local LDAP server on port {}", ldapServer.getIpPort() );
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put( Context.INITIAL_CONTEXT_FACTORY, CTX_FACTORY );
+        env.put( Context.PROVIDER_URL, "ldap://localhost:" + ldapServer.getIpPort() );
+        env.put( Context.SECURITY_PRINCIPAL, ServerDNConstants.ADMIN_SYSTEM_DN );
+        env.put( Context.SECURITY_CREDENTIALS, "secret" );
+        env.put( Context.SECURITY_AUTHENTICATION, "simple" );
+        env.put( Context.REFERRAL, "throw" );
+        return new InitialLdapContext( env, null );
+    }
+
+    
+    /**
+     * Creates a JNDI LdapContext with a connection over the wire using the 
+     * SUN LDAP provider.  The connection is made using the administrative 
+     * user as the principalDN.  The context is to the rootDSE.
+     *
+     * @param ldapServer the LDAP server to get the connection to
+     * @return an LdapContext as the administrative user to the RootDSE
+     * @throws Exception if there are problems creating the context
+     */
+    public static LdapContext getWiredContextFollowOnRefferal( LdapServer ldapServer ) throws Exception
+    {
+        LOG.debug( "Creating a wired context to local LDAP server on port {}", ldapServer.getIpPort() );
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put( Context.INITIAL_CONTEXT_FACTORY, CTX_FACTORY );
+        env.put( Context.PROVIDER_URL, "ldap://localhost:" + ldapServer.getIpPort() );
+        env.put( Context.SECURITY_PRINCIPAL, ServerDNConstants.ADMIN_SYSTEM_DN );
+        env.put( Context.SECURITY_CREDENTIALS, "secret" );
+        env.put( Context.SECURITY_AUTHENTICATION, "simple" );
+        env.put( Context.REFERRAL, "follow" );
+        return new InitialLdapContext( env, null );
+    }
+
+    
+    public static LDAPConnection getWiredConnection( LdapServer ldapServer ) throws Exception
+    {
+        String testServer = System.getProperty( "ldap.test.server", null );
+        
+        if ( testServer == null )
+        {
+            return getWiredConnection( ldapServer, ServerDNConstants.ADMIN_SYSTEM_DN, "secret" );
+        }
+        
+        LOG.debug( "ldap.test.server = " + testServer );
+        
+        String admin = System.getProperty( testServer + ".admin", DEFAULT_ADMIN );
+        LOG.debug( testServer + ".admin = " + admin );
+
+        String password = System.getProperty( testServer + ".password", DEFAULT_PASSWORD );
+        LOG.debug( testServer + ".password = " + password );
+
+        String host = System.getProperty( testServer + ".host", DEFAULT_HOST );
+        LOG.debug( testServer + ".host = " + host );
+
+        int port = Integer.parseInt( System.getProperty( testServer + ".port", Integer.toString( DEFAULT_PORT ) ) );
+        LOG.debug( testServer + ".port = " + port );
+        
+        LDAPConnection conn = new LDAPConnection();
+        conn.connect( 3, host, port, admin, password );
+        return conn;
+    }
+
+    
+    public static LDAPConnection getWiredConnection( LdapServer ldapServer, String principalDn, String password ) 
+        throws Exception
+    {
+        LDAPConnection conn = new LDAPConnection();
+        conn.connect( 3, "localhost", ldapServer.getIpPort(), principalDn, password );
+        return conn;
     }
 }
