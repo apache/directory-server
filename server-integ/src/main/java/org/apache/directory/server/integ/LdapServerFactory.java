ca09a2a500ba71b623bfff233855695bf9077e32
Emmanuel Lecharny <elecharny@apache.org>
2008-11-02 00:41:43 +0000

    Initialize the factory accordingly to MINA 2
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-mina2@709818 13f79535-47bb-0310-9956-ffa450edef68

--- server-integ/src/main/java/org/apache/directory/server/integ/LdapServerFactory.java
+++ server-integ/src/main/java/org/apache/directory/server/integ/LdapServerFactory.java
@@ -21,7 +21,6 @@ package org.apache.directory.server.integ;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
 
 import org.apache.directory.server.core.DefaultDirectoryService;
 import org.apache.directory.server.core.DirectoryService;
@@ -36,7 +35,6 @@ import org.apache.directory.server.ldap.handlers.bind.ntlm.NtlmMechanismHandler;
 import org.apache.directory.server.ldap.handlers.extended.StartTlsHandler;
 import org.apache.directory.server.ldap.handlers.extended.StoredProcedureExtendedOperationHandler;
 import org.apache.directory.shared.ldap.constants.SupportedSaslMechanisms;
-import org.apache.mina.transport.socket.nio.NioProcessor;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.apache.mina.util.AvailablePortFinder;
 
@@ -71,7 +69,7 @@ public interface LdapServerFactory
 
             LdapService ldapService = new LdapService();
             ldapService.setDirectoryService( service );
-            ldapService.setSocketAcceptor( new NioSocketAcceptor( new NioProcessor( Executors.newCachedThreadPool() ) ) );
+            ldapService.setSocketAcceptor( new NioSocketAcceptor() );
             ldapService.setIpPort( AvailablePortFinder.getNextAvailable( 1024 ) );
             ldapService.addExtendedOperationHandler( new StartTlsHandler() );
             ldapService.addExtendedOperationHandler( new StoredProcedureExtendedOperationHandler() );
