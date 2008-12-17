9c78487340bac86356dfdc8aaeed00f13d17471b
Emmanuel Lecharny <elecharny@apache.org>
2008-12-17 15:00:37 +0000

    Added a test for Authenticators checking
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@727404 13f79535-47bb-0310-9956-ffa450edef68

--- server-xml/src/test/java/org/apache/directory/server/SpringServerTest.java
+++ server-xml/src/test/java/org/apache/directory/server/SpringServerTest.java
@@ -19,12 +19,24 @@
 package org.apache.directory.server;
 
 import org.apache.directory.server.configuration.ApacheDS;
+import org.apache.directory.server.core.authn.AuthenticationInterceptor;
+import org.apache.directory.server.core.authn.Authenticator;
+import org.apache.directory.server.core.authn.SimpleAuthenticator;
+import org.apache.directory.server.core.authn.StrongAuthenticator;
+import org.apache.directory.server.core.interceptor.Interceptor;
 import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -32,10 +44,12 @@ import java.net.URL;
  */
 public class SpringServerTest
 {
-    private String providerURL = "dc=example,dc=com";
-
+    /**
+     * Test a default server.xml file 
+     * @throws Exception
+     */
     @Test
-    public void testSpringServerStartup() throws Exception {
+    public void testSpringServerDefault() throws Exception {
         ClassLoader classLoader = this.getClass().getClassLoader();
         URL configURL = classLoader.getResource( "server.xml" );
 
@@ -44,5 +58,51 @@ public class SpringServerTest
         ApacheDS apacheDS = ( ApacheDS ) factory.getBean( "apacheDS" );
         File workingDirFile = new File( configF.getParentFile(), "work" );
         apacheDS.getDirectoryService().setWorkingDirectory( workingDirFile );
+    }
+
+    /**
+     * Test a server.xml with Authenticator in the authenticationInterceptor
+     */
+    @Test
+    public void testSpringServerAuthenticatorInAuthenticationInterceptor() throws Exception {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        URL configURL = classLoader.getResource( "serverAuthenticatorInAuthenticationInterceptor.xml" );
+
+        File configF = new File( configURL.toURI() );
+        ApplicationContext factory = new FileSystemXmlApplicationContext( configF.toURI().toURL().toString() );
+        ApacheDS apacheDS = ( ApacheDS ) factory.getBean( "apacheDS" );
+        File workingDirFile = new File( configF.getParentFile(), "work" );
+        apacheDS.getDirectoryService().setWorkingDirectory( workingDirFile );
+        
+        List<Interceptor> interceptors = apacheDS.getDirectoryService().getInterceptors();
+        
+        Map<String, Interceptor> map = new HashMap<String, Interceptor>();
+        
+        for ( Interceptor interceptor:interceptors )
+        {
+            map.put( interceptor.getName(), interceptor );
+        }
+        
+        Interceptor authentication = map.get( AuthenticationInterceptor.class.getName() );
+        assertNotNull( authentication );
+        Set<Authenticator> authenticators = ((AuthenticationInterceptor)authentication).getAuthenticators();
+        assertNotNull( authenticators );
+        assertEquals( 2, authenticators.size() );
+        int count = 2;
+        
+        for ( Authenticator authenticator: authenticators )
+        {
+            if ( authenticator instanceof SimpleAuthenticator )
+            {
+                count--;
+            }
+            
+            if ( authenticator instanceof StrongAuthenticator )
+            {
+                count--;
+            }
+        }
+        
+        assertEquals( 0, count );
     }
 }
