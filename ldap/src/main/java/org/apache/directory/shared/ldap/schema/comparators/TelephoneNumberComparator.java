aff3af626e29f396a490f12664aed45fc7eab50b
Emmanuel Lecharny <elecharny@apache.org>
2009-01-26 21:55:28 +0000

    Added a comparator for TelephoneNumber
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@737880 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/comparators/TelephoneNumberComparator.java
@@ -0,0 +1,90 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.shared.ldap.schema.comparators;
+
+
+import java.util.Comparator;
+
+
+/**
+ * A comparator for TelephoneNumber.
+ * 
+ * The rules for matching are identical to those for caseIgnoreMatch, except that 
+ * all space and "-" characters are skipped during the comparison. 
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class TelephoneNumberComparator implements Comparator<String>
+{
+    /** A static instance of this comparator */
+    public static final Comparator<String> INSTANCE = new TelephoneNumberComparator();
+    
+    
+    /**
+     * Remove all spaces and '-' from the telephone number
+     */
+    private String strip( String telephoneNumber )
+    {
+        char[] telephoneNumberArray = telephoneNumber.toCharArray();
+        int pos = 0;
+        
+        for ( char c:telephoneNumberArray )
+        {
+            if ( ( c == ' ' ) || ( c == '-' ) )
+            { 
+                continue;
+            }
+            
+            telephoneNumberArray[pos++] = c;
+        }
+        
+        return new String( telephoneNumberArray, 0, pos );
+    }
+
+    
+    /**
+     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+     */
+    public int compare( String telephoneNumber1, String telephoneNumber2 )
+    {
+        // -------------------------------------------------------------------
+        // Handle some basis cases
+        // -------------------------------------------------------------------
+
+        if ( telephoneNumber1 == null )
+        {
+            return ( telephoneNumber2 == null ) ? 0 : -1;
+        }
+        
+        if ( telephoneNumber2 == null )
+        {
+            return 1;
+        }
+        
+        // -------------------------------------------------------------------
+        // Remove all spaces and '-'
+        // -------------------------------------------------------------------
+        telephoneNumber1 = strip( telephoneNumber1 );
+        telephoneNumber2 = strip( telephoneNumber2 );
+        
+        return ( telephoneNumber1.compareTo( telephoneNumber2 ) );
+    }
+}
---
+++ ldap/src/test/java/org/apache/directory/shared/ldap/schema/comparators/TelephoneNumberComparatorTest.java
@@ -0,0 +1,105 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.shared.ldap.schema.comparators;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Test the TelephoneNumber comparator
+ * 
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class TelephoneNumberComparatorTest
+{
+    private TelephoneNumberComparator comparator;
+    
+    @Before
+    public void init()
+    {
+        comparator = new TelephoneNumberComparator();
+    }
+    
+    
+    @Test
+    public void testNullTelephoneNumbers()
+    {
+        String tel1 = null;
+        String tel2 = null;
+        
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+        
+        tel2 = "abc";
+        assertEquals( -1, comparator.compare( tel1, tel2 ) );
+
+        String tel3 = null;
+        assertEquals( 1, comparator.compare( tel2, tel3 ) );
+    }
+
+
+    @Test
+    public void testEmptyTelephoneNumbers()
+    {
+        String tel1 = "";
+        String tel2 = "";
+        
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+        
+        tel2 = "abc";
+        assertTrue( comparator.compare( tel1, tel2 ) < 0 );
+
+        String tel3 = "";
+        assertTrue( comparator.compare( tel2, tel3 ) > 0 );
+    }
+    
+    
+    @Test
+    public void testSimpleTelephoneNumbers()
+    {
+        String tel1 = "01 02 03 04 05";
+        String tel2 = "01 02 03 04 05";
+        
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+        
+        tel2 = "0102030405";
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+    }
+    
+    
+    @Test
+    public void testComplexTelephoneNumbers()
+    {
+        String tel1 = "  + 33 1 01-02-03-04-05  ";
+        String tel2 = "+3310102030405";
+        
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+        
+        tel1 = "1-801-555-1212";
+        tel2 = "18015551212";
+
+        assertEquals( 0, comparator.compare( tel1, tel2 ) );
+        assertEquals( 0, comparator.compare( "1 801 555 1212", tel1 ) );
+        assertEquals( 0, comparator.compare( "1 801 555 1212", tel2 ) );
+    }
+}
