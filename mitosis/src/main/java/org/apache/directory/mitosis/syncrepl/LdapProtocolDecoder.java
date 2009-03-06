e82b7f215884ba5e71805bfbe766b5a0b4c75972
Emmanuel Lecharny <elecharny@apache.org>
2009-03-06 01:03:10 +0000

    Fixed the decoder to accept splitted buffers. Added some logs
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-replication@750721 13f79535-47bb-0310-9956-ffa450edef68

--- mitosis/src/main/java/org/apache/directory/mitosis/syncrepl/LdapProtocolDecoder.java
+++ mitosis/src/main/java/org/apache/directory/mitosis/syncrepl/LdapProtocolDecoder.java
@@ -19,16 +19,22 @@
  */
 package org.apache.directory.mitosis.syncrepl;
 
+import java.nio.ByteBuffer;
+
 import org.apache.directory.shared.asn1.ber.Asn1Decoder;
 import org.apache.directory.shared.asn1.ber.IAsn1Container;
+import org.apache.directory.shared.asn1.ber.tlv.TLVStateEnum;
 import org.apache.directory.shared.asn1.codec.DecoderException;
 import org.apache.directory.shared.ldap.codec.LdapDecoder;
 import org.apache.directory.shared.ldap.codec.LdapMessage;
 import org.apache.directory.shared.ldap.codec.LdapMessageContainer;
+import org.apache.directory.shared.ldap.util.StringTools;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -39,6 +45,12 @@ import org.apache.mina.filter.codec.ProtocolDecoderOutput;
  */
 public class LdapProtocolDecoder implements ProtocolDecoder
 {
+    /** The logger */
+    private static final Logger LOG = LoggerFactory.getLogger( LdapProtocolDecoder.class );
+    
+    /** A speedup for logger */
+    private static final boolean IS_DEBUG = LOG.isDebugEnabled();
+
     /**
      * Decode a Ldap request and write it to the remote server.
      * 
@@ -52,26 +64,55 @@ public class LdapProtocolDecoder implements ProtocolDecoder
         // Allocate a LdapMessage Container
         Asn1Decoder ldapDecoder = new LdapDecoder();
         IAsn1Container ldapMessageContainer = (LdapMessageContainer)session.getAttribute( "LDAP-Container" );
+    	ByteBuffer buf = buffer.buf();
 
-        // Decode a LDAP PDU
-        try
+        
+        while ( buf.hasRemaining() )
         {
-            ldapDecoder.decode( buffer.buf(), ldapMessageContainer );
+            try
+            {
+                ldapDecoder.decode( buf, ldapMessageContainer );
+    
+                if ( IS_DEBUG )
+                {
+                    LOG.debug( "Decoding the PDU : " );
+                	int size = buf.capacity();
+                	
+                	byte[] b = new byte[size];
+                	
+                	System.arraycopy( buf.array(), 0, b, 0, size );
+                	
+                	System.out.println( "Received buffer : " + StringTools.dumpBytes( b ) );
+                }
+                
+                if ( ldapMessageContainer.getState() == TLVStateEnum.PDU_DECODED )
+                {
+                    // get back the decoded message
+                    LdapMessage message = ( ( LdapMessageContainer ) ldapMessageContainer ).getLdapMessage();
+                    
+                    if ( IS_DEBUG )
+                    {
+                        LOG.debug( "Decoded LdapMessage : " + 
+                        		( ( LdapMessageContainer ) ldapMessageContainer ).getLdapMessage() );
+                        buf.mark();
+                    }
+    
+                    // Clean the container for the next decoding
+                    ( ( LdapMessageContainer ) ldapMessageContainer).clean();
+                    
+                    System.out.println( "Decoded message : " + message );
+                    
+                    // Send back the message
+                    out.write( message );
+                }
+            }
+            catch ( DecoderException de )
+            {
+                buf.clear();
+                ( ( LdapMessageContainer ) ldapMessageContainer).clean();
+                throw de;
+            }
         }
-        catch ( DecoderException de )
-        {
-            de.printStackTrace();
-        }
-        
-        // get back the decoded message
-        LdapMessage message = ( ( LdapMessageContainer ) ldapMessageContainer ).getLdapMessage();
-        
-        // Clean the container for the next decoding
-        ( ( LdapMessageContainer ) ldapMessageContainer).clean();
-        
-        // Send back the message
-        out.write( message );
-        
     }
     
     
