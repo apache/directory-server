db93412fe4749398272067878ee4a97fd4d4e970
Emmanuel Lecharny <elecharny@apache.org>
2009-03-30 18:24:30 +0000

    Changed the code to compile with Cursor in shared
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-replication@760089 13f79535-47bb-0310-9956-ffa450edef68

--- xdbm-search/src/main/java/org/apache/directory/server/xdbm/search/impl/OneLevelScopeCursor.java
+++ xdbm-search/src/main/java/org/apache/directory/server/xdbm/search/impl/OneLevelScopeCursor.java
@@ -20,13 +20,13 @@
 package org.apache.directory.server.xdbm.search.impl;
 
 
-import org.apache.directory.server.core.cursor.Cursor;
-import org.apache.directory.server.core.cursor.InvalidCursorPositionException;
 import org.apache.directory.server.core.entry.ServerEntry;
 import org.apache.directory.server.xdbm.IndexEntry;
 import org.apache.directory.server.xdbm.Store;
 import org.apache.directory.server.xdbm.AbstractIndexCursor;
 import org.apache.directory.server.xdbm.IndexCursor;
+import org.apache.directory.shared.ldap.cursor.Cursor;
+import org.apache.directory.shared.ldap.cursor.InvalidCursorPositionException;
 
 
 /**
