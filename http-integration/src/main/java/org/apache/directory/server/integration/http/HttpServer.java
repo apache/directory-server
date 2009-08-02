4b13c19f8ebcc26965b8f4896ce098840c22836f
Kiran Ayyagari <kayyagari@apache.org>
2009-08-02 18:23:33 +0000

    integrated jetty http server(DIRSERVER-1262)
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@800123 13f79535-47bb-0310-9956-ffa450edef68

---
+++ http-integration/src/main/java/org/apache/directory/server/integration/http/HttpServer.java
@@ -0,0 +1,201 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+
+package org.apache.directory.server.integration.http;
+
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.xml.XmlConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Class to start the jetty http server
+ * 
+ * @org.apache.xbean.XBean
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$, $Date$
+ */
+public class HttpServer
+{
+
+    /** the jetty http server instance */
+    private Server jetty;
+
+    /** jetty config file */
+    private String confFile;
+
+    /** a collection to hold the configured web applications */
+    private Set<WebApp> webApps;
+
+    private int port = 8080;
+
+    private boolean configured = false;
+
+    private static final Logger LOG = LoggerFactory.getLogger( HttpServer.class );
+
+
+    public HttpServer()
+    {
+    }
+
+
+    /**
+     * starts the jetty http server
+     * 
+     * @throws Exception
+     */
+    public void start() throws Exception
+    {
+
+        if ( confFile == null && ( webApps == null || webApps.isEmpty() ) )
+        {
+            LOG.warn( "Neither configuration file nor web apps were configured for the http server, skipping initialization." );
+            return;
+        }
+
+        XmlConfiguration jettyConf = null;
+
+        if ( confFile != null )
+        {
+            jettyConf = new XmlConfiguration( new FileInputStream( confFile ) );
+
+            LOG.info( "configuring jetty http server from the configuration file {}", confFile );
+
+            try
+            {
+                jetty = new Server();
+                jettyConf.configure( jetty );
+                configured = true;
+            }
+            catch ( Exception e )
+            {
+                LOG.error( "Failed to configure the jetty http server" );
+                throw e;
+            }
+        }
+        else
+        {
+            LOG.info( "No configuration file set, looking for web apps" );
+            configureServerThroughCode();
+        }
+
+        if ( configured )
+        {
+            LOG.info( "starting jetty http server" );
+            jetty.start();
+        }
+        else
+        {
+            jetty = null;
+            LOG.warn( "Error while configuring the http server, skipping the http server startup" );
+        }
+    }
+
+
+    /*
+     * configure the jetty server programmatically without using any configuration file 
+     */
+    private void configureServerThroughCode()
+    {
+        try
+        {
+            jetty = new Server();
+
+            Connector connector = new SelectChannelConnector();
+            connector.setPort( port );
+            jetty.setConnectors( new Connector[]{ connector } );
+
+            List<Handler> handlers = new ArrayList<Handler>();
+            for ( WebApp w : webApps )
+            {
+                WebAppContext webapp = new WebAppContext();
+                webapp.setWar( w.getWarFile() );
+                webapp.setContextPath( w.getContextPath() );
+                handlers.add( webapp );
+            }
+
+            jetty.setHandlers( handlers.toArray( new Handler[ handlers.size() ] ) );
+            
+            configured = true;
+        }
+        catch ( Exception e )
+        {
+            LOG.error( "Failed to configure the web application(s)", e );
+        }
+
+    }
+
+
+    /**
+     * stops the jetty http server
+     * 
+     * @throws Exception
+     */
+    public void stop() throws Exception
+    {
+        if ( jetty != null && jetty.isStarted() )
+        {
+            LOG.info( "stopping jetty http server" );
+            jetty.stop();
+        }
+    }
+
+
+    public void setConfFile( String confFile )
+    {
+        this.confFile = confFile;
+    }
+
+
+    public Set<WebApp> getWebApps()
+    {
+        return webApps;
+    }
+
+
+    public void setWebApps( Set<WebApp> webapps )
+    {
+        this.webApps = webapps;
+    }
+
+
+    public int getPort()
+    {
+        return port;
+    }
+
+
+    public void setPort( int port )
+    {
+        this.port = port;
+    }
+
+}
