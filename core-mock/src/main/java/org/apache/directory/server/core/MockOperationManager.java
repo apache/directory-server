651188bd8c050279385b35a1722b41f1d5584c9f
Emmanuel Lecharny <elecharny@apache.org>
2009-09-06 01:46:37 +0000

    Added ASF headers Renamed a class
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-schema@811744 13f79535-47bb-0310-9956-ffa450edef68

--- core-mock/src/main/java/org/apache/directory/server/core/MockOperationManager.java
+++ core-mock/src/main/java/org/apache/directory/server/core/MockOperationManager.java
@@ -1,4 +1,22 @@
-
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
 package org.apache.directory.server.core;
 
 import java.util.Set;
@@ -107,7 +125,7 @@ class MockOperationManager implements OperationManager
 
     public EntryFilteringCursor search( SearchOperationContext opContext ) throws Exception
     {
-        return new BaseEntryFilteringCursor( new BogusCursor( count ), opContext );
+        return new BaseEntryFilteringCursor( new MockCursor( count ), opContext );
     }
 
 
