b4a73d0b64823168d74432629643c6cd24bf88f5
Emmanuel Lecharny <elecharny@apache.org>
2009-12-22 23:27:09 +0000

    o Used a LdifLoader instead of a Jar loader o Minor cleanup
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-schema@893348 13f79535-47bb-0310-9956-ffa450edef68

--- core-integ/src/test/java/org/apache/directory/server/core/authz/AutzIntegUtils.java
+++ core-integ/src/test/java/org/apache/directory/server/core/authz/AutzIntegUtils.java
@@ -55,9 +55,11 @@ import org.apache.directory.shared.ldap.constants.SchemaConstants;
 import org.apache.directory.shared.ldap.name.LdapDN;
 import org.apache.directory.shared.ldap.schema.SchemaManager;
 import org.apache.directory.shared.ldap.schema.ldif.extractor.SchemaLdifExtractor;
+import org.apache.directory.shared.ldap.schema.registries.SchemaLoader;
 import org.apache.directory.shared.ldap.util.ExceptionUtils;
 import org.apache.directory.shared.schema.DefaultSchemaManager;
 import org.apache.directory.shared.schema.loader.ldif.JarLdifSchemaLoader;
+import org.apache.directory.shared.schema.loader.ldif.LdifSchemaLoader;
 
 
 /**
@@ -105,10 +107,11 @@ public class AutzIntegUtils
             // Extract the schema on disk (a brand new one) and load the registries
             File schemaRepository = new File( workingDirectory, "schema" );
             SchemaLdifExtractor extractor = new SchemaLdifExtractor( new File( workingDirectory ) );
+            extractor.extractOrCopy();
 
             schemaPartition.setWrappedPartition( ldifPartition );
 
-            JarLdifSchemaLoader loader = new JarLdifSchemaLoader();
+            SchemaLoader loader = new LdifSchemaLoader( schemaRepository );
 
             SchemaManager schemaManager = new DefaultSchemaManager( loader );
             service.setSchemaManager( schemaManager );
@@ -123,8 +126,6 @@ public class AutzIntegUtils
             }
 
             schemaPartition.setSchemaManager( schemaManager );
-
-            extractor.extractOrCopy();
 
             service.getChangeLog().setEnabled( true );
 
@@ -147,7 +148,6 @@ public class AutzIntegUtils
 
             service.setSystemPartition( systemPartition );
             service.setAccessControlEnabled( true );
-            AutzIntegUtils.service = service;
         }
     }
 
