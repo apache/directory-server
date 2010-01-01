8291868d5a92f6cde0ef039bc439c4a1a0041ec6
Kiran Ayyagari <kayyagari@apache.org>
2010-01-01 15:40:43 +0000

    o added support for creating KdcServer using annotation o added dependency on apacheds-protocol-kerberos o added a test class for testing CreateKdcServer annotation
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-schema@895027 13f79535-47bb-0310-9956-ffa450edef68

--- server-annotations/src/main/java/org/apache/directory/server/factory/ServerAnnotationProcessor.java
+++ server-annotations/src/main/java/org/apache/directory/server/factory/ServerAnnotationProcessor.java
@@ -18,20 +18,24 @@
  */
 package org.apache.directory.server.factory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
+import org.apache.directory.server.annotations.CreateKdcServer;
 import org.apache.directory.server.annotations.CreateLdapServer;
 import org.apache.directory.server.annotations.CreateTransport;
 import org.apache.directory.server.core.DirectoryService;
+import org.apache.directory.server.kerberos.kdc.KdcServer;
 import org.apache.directory.server.ldap.LdapServer;
 import org.apache.directory.server.protocol.shared.transport.TcpTransport;
 import org.apache.directory.server.protocol.shared.transport.Transport;
+import org.apache.directory.server.protocol.shared.transport.UdpTransport;
 import org.apache.mina.util.AvailablePortFinder;
 import org.junit.runner.Description;
 
 /**
  * 
- * TODO ServerAnnotationProcessor.
+ * Annotation processor for creating LDAP and Kerberos servers.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  * @version $Rev$, $Date$
@@ -68,6 +72,10 @@ public class ServerAnnotationProcessor
                     Transport ldaps = new TcpTransport( address, port, nbThreads, backlog );
                     ldaps.setEnableSSL( true );
                     ldapServer.addTransports( ldaps );
+                }
+                else
+                {
+                    throw new IllegalArgumentException( "Unsupported protocol type " + protocol );
                 }
             }
         }
@@ -121,38 +129,7 @@ public class ServerAnnotationProcessor
     
     public static LdapServer getLdapServer( DirectoryService directoryService, int startPort ) throws Exception
     {
-        CreateLdapServer createLdapServer = null;
-        
-        // Get the caller by inspecting the stackTrace
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        
-        // Get the enclosing class
-        Class<?> classCaller = Class.forName( stackTrace[2].getClassName() );
-        
-        // Get the current method
-        String methodCaller = stackTrace[2].getMethodName();
-        
-        // Check if we have any annotation associated with the method
-        Method[] methods = classCaller.getMethods();
-        
-        for ( Method method : methods )
-        {
-            if ( methodCaller.equals( method.getName() ) )
-            {
-                createLdapServer = method.getAnnotation( CreateLdapServer.class );
-                
-                if ( createLdapServer != null )
-                {
-                    break;
-                }
-            }
-        }
-
-        // No : look at the class level
-        if ( createLdapServer == null )
-        {
-            createLdapServer = classCaller.getAnnotation( CreateLdapServer.class );
-        }
+        CreateLdapServer createLdapServer = ( CreateLdapServer ) getAnnotation( CreateLdapServer.class );
         
         // Ok, we have found a CreateLdapServer annotation. Process it now.
         return createLdapServer( createLdapServer, directoryService, startPort );
@@ -166,4 +143,124 @@ public class ServerAnnotationProcessor
         // Ok, we have found a CreateLdapServer annotation. Process it now.
         return createLdapServer( createLdapServer, directoryService, startPort );
     }
+
+    @SuppressWarnings("unchecked")
+    private static Annotation getAnnotation( Class annotationClass ) throws Exception
+    {
+        // Get the caller by inspecting the stackTrace
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        
+        // Get the enclosing class
+        Class<?> classCaller = Class.forName( stackTrace[3].getClassName() );
+
+        // Get the current method
+        String methodCaller = stackTrace[3].getMethodName();
+
+        // Check if we have any annotation associated with the method
+        Method[] methods = classCaller.getMethods();
+        
+        for ( Method method : methods )
+        {
+            if ( methodCaller.equals( method.getName() ) )
+            {
+                Annotation annotation = method.getAnnotation( annotationClass );
+                
+                if ( annotation != null )
+                {
+                    return annotation;
+                }
+            }
+        }
+        
+        // No : look at the class level
+        return classCaller.getAnnotation( annotationClass );
+    }
+    
+    
+    public static KdcServer getKdcServer( DirectoryService directoryService, int startPort ) throws Exception
+    {
+        CreateKdcServer createKdcServer = ( CreateKdcServer ) getAnnotation( CreateKdcServer.class );
+
+        return createKdcServer( createKdcServer, directoryService, startPort );
+    }
+
+    
+    private static KdcServer createKdcServer( CreateKdcServer createKdcServer, DirectoryService directoryService, int startPort )
+    {
+        if( createKdcServer == null )
+        {
+            return null;
+        }
+        
+        KdcServer kdcServer = new KdcServer();
+        kdcServer.setServiceName( createKdcServer.name() );
+        kdcServer.setKdcPrincipal( createKdcServer.kdcPrincipal() );
+        kdcServer.setPrimaryRealm( createKdcServer.primaryRealm() );
+        kdcServer.setMaximumTicketLifetime( createKdcServer.maxTicketLifetime() );
+        kdcServer.setMaximumRenewableLifetime( createKdcServer.maxRenewableLifetime() );
+        
+        CreateTransport[] transportBuilders = createKdcServer.transports();
+        
+        if( transportBuilders == null )
+        {
+            // create only UDP transport if none specified
+            UdpTransport defaultTransport = new UdpTransport( AvailablePortFinder.getNextAvailable( startPort ) );
+            kdcServer.addTransports( defaultTransport );
+        }
+        else if( transportBuilders.length > 0 )
+        {
+            for( CreateTransport transportBuilder : transportBuilders )
+            {
+                String protocol = transportBuilder.protocol();
+                int port = transportBuilder.port();
+                int nbThreads = transportBuilder.nbThreads();
+                int backlog = transportBuilder.backlog();
+                String address = transportBuilder.address();
+
+                if ( port == -1 )
+                {
+                    port = AvailablePortFinder.getNextAvailable( startPort );
+                    startPort = port + 1;
+                }
+                
+                if ( protocol.equalsIgnoreCase( "TCP" ) )
+                {
+                    Transport tcp = new TcpTransport( address, port, nbThreads, backlog );
+                    kdcServer.addTransports( tcp );
+                }
+                else if ( protocol.equalsIgnoreCase( "UDP" ) )
+                {
+                    UdpTransport udp = new UdpTransport( address, port );
+                    kdcServer.addTransports( udp );
+                }
+                else
+                {
+                    throw new IllegalArgumentException( "Unsupported protocol type " + protocol );
+                }
+            }
+        }
+        
+        kdcServer.setDirectoryService( directoryService );
+        
+        // Launch the server
+        try
+        {
+            kdcServer.start();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+        
+        return kdcServer;
+    }
+    
+    
+    public static KdcServer getKdcServer( Description description, DirectoryService directoryService, int startPort ) throws Exception
+    {
+        CreateKdcServer createLdapServer = description.getAnnotation( CreateKdcServer.class );
+
+        return createKdcServer( createLdapServer, directoryService, startPort );
+    }
+
 }
