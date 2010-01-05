8f9aa7ef07ec4f329c84fbde521135eaf56fa14b
Emmanuel Lecharny <elecharny@apache.org>
2010-01-05 02:13:31 +0000

    Added all the missing tests n the suite
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-schema@895853 13f79535-47bb-0310-9956-ffa450edef68

--- server-integ/src/test/java/org/apache/directory/server/suites/StockServerISuite.java
+++ server-integ/src/test/java/org/apache/directory/server/suites/StockServerISuite.java
@@ -30,24 +30,32 @@ import org.apache.directory.server.core.integ.FrameworkSuite;
 import org.apache.directory.server.operations.add.AddIT;
 import org.apache.directory.server.operations.add.AddingEntriesWithSpecialCharactersInRDNIT;
 import org.apache.directory.server.operations.bind.BindIT;
+import org.apache.directory.server.operations.bind.MiscBindIT;
+import org.apache.directory.server.operations.bind.SaslBindIT;
 import org.apache.directory.server.operations.bind.SimpleBindIT;
 import org.apache.directory.server.operations.compare.CompareIT;
 import org.apache.directory.server.operations.compare.MatchingRuleCompareIT;
 import org.apache.directory.server.operations.delete.DeleteIT;
+import org.apache.directory.server.operations.extended.ExtendedIT;
+import org.apache.directory.server.operations.extended.StoredProcedureIT;
 import org.apache.directory.server.operations.modify.IllegalModificationIT;
 import org.apache.directory.server.operations.modify.ModifyAddIT;
+import org.apache.directory.server.operations.modify.ModifyMultipleChangesIT;
 import org.apache.directory.server.operations.modify.ModifyReferralIT;
 import org.apache.directory.server.operations.modify.ModifyRemoveIT;
 import org.apache.directory.server.operations.modify.ModifyReplaceIT;
 import org.apache.directory.server.operations.modifydn.ModifyDnReferralIT;
 import org.apache.directory.server.operations.modifydn.ModifyRdnIT;
 import org.apache.directory.server.operations.modifydn.MoveIT;
+import org.apache.directory.server.operations.search.IndexedNegationSearchIT;
 import org.apache.directory.server.operations.search.NegationSearchIT;
+import org.apache.directory.server.operations.search.PagedSearchIT;
 import org.apache.directory.server.operations.search.PersistentSearchIT;
 import org.apache.directory.server.operations.search.ReferralSearchIT;
 import org.apache.directory.server.operations.search.SchemaSearchIT;
 import org.apache.directory.server.operations.search.SearchIT;
 import org.apache.directory.server.operations.search.SearchLimitsIT;
+import org.apache.directory.server.ssl.LdapsIT;
 import org.apache.directory.server.ssl.StartTlsIT;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -63,26 +71,41 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses ( {
         AddingEntriesWithSpecialCharactersInRDNIT.class,
         AddIT.class,
+
+        BindIT.class,
+        MiscBindIT.class,
+        SaslBindIT.class,
+        SimpleBindIT.class,
+
         CompareIT.class,
         MatchingRuleCompareIT.class,
+        
         DeleteIT.class,
+        ExtendedIT.class,
+        StoredProcedureIT.class,
+        
         IllegalModificationIT.class,
         ModifyAddIT.class,
+        ModifyMultipleChangesIT.class,
         ModifyReferralIT.class,
         ModifyRemoveIT.class,
         ModifyReplaceIT.class,
-        ModifyRdnIT.class,
-        BindIT.class,
-        SimpleBindIT.class,
-        MoveIT.class,
-        SearchIT.class,
-        NegationSearchIT.class,
-        SchemaSearchIT.class,
-        ReferralSearchIT.class,
-        StartTlsIT.class,
+
         ModifyDnReferralIT.class,
+        ModifyRdnIT.class,
+        MoveIT.class,
+
+        IndexedNegationSearchIT.class,
+        NegationSearchIT.class,
+        PagedSearchIT.class,
         PersistentSearchIT.class,
-        SearchLimitsIT.class
+        ReferralSearchIT.class,
+        SchemaSearchIT.class,
+        SearchIT.class,
+        SearchLimitsIT.class,
+
+        LdapsIT.class,
+        StartTlsIT.class
         } )
 @CreateDS( 
     name = "SuiteDS",
