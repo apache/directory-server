bbb7037ef5a9b6eef677910c976fac71a4cabf1f
Stefan Seelmann <seelmann@apache.org>
2010-03-31 18:20:24 +0000

    Added license header and class javadoc
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@929651 13f79535-47bb-0310-9956-ffa450edef68

--- core-api/src/main/java/org/apache/directory/server/core/partition/PartitionNexus.java
+++ core-api/src/main/java/org/apache/directory/server/core/partition/PartitionNexus.java
@@ -1,3 +1,22 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
 package org.apache.directory.server.core.partition;
 
 
@@ -15,6 +34,15 @@ import org.apache.directory.shared.ldap.name.DN;
 import org.apache.directory.shared.ldap.util.StringTools;
 
 
+/**
+ * A root {@link Partition} that contains all other partitions, and
+ * routes all operations to the child partition that matches to its base suffixes.
+ * It also provides some extended operations such as accessing rootDSE and
+ * listing base suffixes.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev: 927404 $, $Date: 2010-03-25 14:55:18 +0100 (Do, 25. Mär 2010) $
+ */
 public interface PartitionNexus extends Partition
 {
 
