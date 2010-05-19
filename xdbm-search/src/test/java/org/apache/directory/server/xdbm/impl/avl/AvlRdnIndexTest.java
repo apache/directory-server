993248dc455ae14e41806d9e157d3b6a9f3f376f
Stefan Seelmann <seelmann@apache.org>
2010-05-19 20:16:17 +0000

    Reapplied changes from commit 946132. They disappeard during merge due to tree conflicts.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@946367 13f79535-47bb-0310-9956-ffa450edef68

--- xdbm-search/src/test/java/org/apache/directory/server/xdbm/impl/avl/AvlRdnIndexTest.java
+++ xdbm-search/src/test/java/org/apache/directory/server/xdbm/impl/avl/AvlRdnIndexTest.java
@@ -41,7 +41,7 @@ import org.apache.directory.shared.ldap.schema.ldif.extractor.SchemaLdifExtracto
 import org.apache.directory.shared.ldap.schema.ldif.extractor.impl.DefaultSchemaLdifExtractor;
 import org.apache.directory.shared.ldap.schema.loader.ldif.LdifSchemaLoader;
 import org.apache.directory.shared.ldap.schema.manager.impl.DefaultSchemaManager;
-import org.apache.directory.shared.ldap.util.ExceptionUtils;
+import org.apache.directory.shared.ldap.util.LdapExceptionUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -83,7 +83,7 @@ public class AvlRdnIndexTest
 
         if ( !loaded )
         {
-            fail( "Schema load failed : " + ExceptionUtils.printErrors( schemaManager.getErrors() ) );
+            fail( "Schema load failed : " + LdapExceptionUtils.printErrors( schemaManager.getErrors() ) );
         }
     }
 
--- xdbm-search/src/test/java/org/apache/directory/server/xdbm/impl/avl/AvlStoreTest.java
+++ xdbm-search/src/test/java/org/apache/directory/server/xdbm/impl/avl/AvlStoreTest.java
@@ -64,7 +64,7 @@ import org.apache.directory.shared.ldap.schema.ldif.extractor.SchemaLdifExtracto
 import org.apache.directory.shared.ldap.schema.ldif.extractor.impl.DefaultSchemaLdifExtractor;
 import org.apache.directory.shared.ldap.schema.loader.ldif.LdifSchemaLoader;
 import org.apache.directory.shared.ldap.schema.manager.impl.DefaultSchemaManager;
-import org.apache.directory.shared.ldap.util.ExceptionUtils;
+import org.apache.directory.shared.ldap.util.LdapExceptionUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -113,7 +113,7 @@ public class AvlStoreTest
 
         if ( !loaded )
         {
-            fail( "Schema load failed : " + ExceptionUtils.printErrors( schemaManager.getErrors() ) );
+            fail( "Schema load failed : " + LdapExceptionUtils.printErrors( schemaManager.getErrors() ) );
         }
 
         EXAMPLE_COM = new DN( "dc=example,dc=com" );
