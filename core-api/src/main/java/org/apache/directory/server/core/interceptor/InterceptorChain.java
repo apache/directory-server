1d1d774daa50e6b29908efac4b38cb55ffa0b982
Emmanuel Lecharny <elecharny@apache.org>
2010-06-06 21:20:58 +0000

    o Added a perf test for the List operation o Removed the eagerlyPopulateFields for this operation
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@951979 13f79535-47bb-0310-9956-ffa450edef68

--- core-api/src/main/java/org/apache/directory/server/core/interceptor/InterceptorChain.java
+++ core-api/src/main/java/org/apache/directory/server/core/interceptor/InterceptorChain.java
@@ -691,7 +691,6 @@ public class InterceptorChain
         Element entry = getStartingEntry();
         Interceptor head = entry.interceptor;
         NextInterceptor next = entry.nextInterceptor;
-        eagerlyPopulateFields( opContext );
 
         try
         {
