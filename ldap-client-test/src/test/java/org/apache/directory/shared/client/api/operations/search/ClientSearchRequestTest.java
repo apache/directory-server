b180cfd4d7e682cff9bc51449a55f1c4a0d6def0
Kiran Ayyagari <kayyagari@apache.org>
2010-07-19 17:40:02 +0000

    o removed static initialization to avoid NPE while running in eclipse
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@965570 13f79535-47bb-0310-9956-ffa450edef68

--- ldap-client-test/src/test/java/org/apache/directory/shared/client/api/operations/search/ClientSearchRequestTest.java
+++ ldap-client-test/src/test/java/org/apache/directory/shared/client/api/operations/search/ClientSearchRequestTest.java
@@ -46,8 +46,8 @@ import org.apache.directory.shared.ldap.filter.SearchScope;
 import org.apache.directory.shared.ldap.message.AliasDerefMode;
 import org.apache.directory.shared.ldap.message.ResultCodeEnum;
 import org.apache.directory.shared.ldap.name.DN;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -81,10 +81,10 @@ import org.junit.runner.RunWith;
 })
 public class ClientSearchRequestTest extends AbstractLdapTestUnit
 {
-    private static LdapAsyncConnection connection;
+    private LdapAsyncConnection connection;
 
-    @BeforeClass
-    public static void setup() throws Exception
+    @Before
+    public void setup() throws Exception
     {
         connection = new LdapNetworkConnection( "localhost", ldapServer.getPort() );
         DN bindDn = new DN( "uid=admin,ou=system" );
@@ -95,8 +95,8 @@ public class ClientSearchRequestTest extends AbstractLdapTestUnit
     /**
      * Close the LdapConnection
      */
-    @AfterClass
-    public static void shutdown()
+    @After
+    public void shutdown()
     {
         try
         {
