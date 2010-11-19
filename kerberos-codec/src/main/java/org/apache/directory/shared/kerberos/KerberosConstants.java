8c85b8abf1848d4b1d8e8ca8693c2191aea007ab
Emmanuel Lecharny <elecharny@apache.org>
2010-11-19 14:09:59 +0000

    o Added the Authenticator tags o Shuffled the the declaration, separating messages from components and ordering them by message type and alphabetical order
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1036852 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-codec/src/main/java/org/apache/directory/shared/kerberos/KerberosConstants.java
+++ kerberos-codec/src/main/java/org/apache/directory/shared/kerberos/KerberosConstants.java
@@ -28,7 +28,10 @@ public class KerberosConstants
 {
     /** The Kerberos version 5 */
     public static final int KERBEROS_V5 = 5;
-    
+
+    //-------------------------------------------------------------------------
+    // Messages
+    //-------------------------------------------------------------------------
     /** Ticket message's tags */
     public static final int TICKET_TAG = 0x61;
     public static final int TICKET_TKT_VNO_TAG = 0xA0;
@@ -36,63 +39,17 @@ public class KerberosConstants
     public static final int TICKET_SNAME_TAG = 0xA2;
     public static final int TICKET_ENC_PART_TAG = 0xA3;
     
-    /** PrincipalName's tags */
-    public static final int PRINCIPAL_NAME_NAME_TYPE_TAG = 0xA0;
-    public static final int PRINCIPAL_NAME_NAME_STRING_TAG = 0xA1;
-    
-    /** Encrypteddata's tags */
-    public static final int ENCRYPTED_DATA_ETYPE_TAG = 0xA0;
-    public static final int ENCRYPTED_DATA_KVNO_TAG = 0xA1;
-    public static final int ENCRYPTED_DATA_CIPHER_TAG = 0xA2;
-    
-    /** HostAddress' tags */
-    public static final int HOST_ADDRESS_ADDR_TYPE_TAG = 0xA0;
-    public static final int HOST_ADDRESS_ADDRESS_TAG = 0xA1;
-    
-    /** AuthorizationData tags */
-    public static final int AUTHORIZATION_DATA_ADTYPE_TAG = 0xA0;
-    public static final int AUTHORIZATION_DATA_ADDATA_TAG = 0xA1;
-
-    /** Checksum tags */
-    public static final int CHECKSUM_TYPE_TAG = 0xA0;
-    public static final int CHECKSUM_CHECKSUM_TAG = 0xA1;
-
-    /** PaData tags */
-    public static final int PADATA_TYPE_TAG = 0xA1;
-    public static final int PADATA_VALUE_TAG = 0xA2;
-
-    /** EncryptionKey tags */
-    public static final int ENCRYPTION_KEY_TYPE_TAG = 0xA0;
-    public static final int ENCRYPTION_KEY_VALUE_TAG = 0xA1;
-
-    /** KRB-REQ-BODY's tags */
-    public static final int KDC_REQ_BODY_KDC_OPTIONS_TAG = 0xA0;
-    public static final int KDC_REQ_BODY_CNAME_TAG = 0xA1;
-    public static final int KDC_REQ_BODY_REALM_TAG = 0xA2;
-    public static final int KDC_REQ_BODY_SNAME_TAG = 0xA3;
-    public static final int KDC_REQ_BODY_FROM_TAG = 0xA4;
-    public static final int KDC_REQ_BODY_TILL_TAG = 0xA5;
-    public static final int KDC_REQ_BODY_RTIME_TAG = 0xA6;
-    public static final int KDC_REQ_BODY_NONCE_TAG = 0xA7;
-    public static final int KDC_REQ_BODY_ETYPE_TAG = 0xA8;
-    public static final int KDC_REQ_BODY_ADDRESSES_TAG = 0xA9;
-    public static final int KDC_REQ_BODY_ENC_AUTHZ_DATA_TAG = 0xAA;
-    public static final int KDC_REQ_BODY_ADDITIONAL_TICKETS_TAG = 0xAB;
-
-    /** KRB-REQ's tags */
-    public static final int KDC_REQ_PVNO_TAG = 0xA1;
-    public static final int KDC_REQ_MSG_TYPE_TAG = 0xA2;
-    public static final int KDC_REQ_PA_DATA_TAG = 0xA3;
-    public static final int KDC_REQ_KDC_REQ_BODY_TAG = 0xA4;
-
-    /** KRB-REP's tags */
-    public static final int KDC_REP_PVNO_TAG = 0xA0;
-    public static final int KDC_REP_MSG_TYPE_TAG = 0xA1;
-    public static final int KDC_REP_PA_DATA_TAG = 0xA2;
-    public static final int KDC_REP_CREALM_TAG = 0xA3;
-    public static final int KDC_REP_CNAME_TAG = 0xA4;
-    public static final int KDC_REP_TICKET_TAG = 0xA5;
-    public static final int KDC_REP_ENC_PART_TAG = 0xA6;
+    /** Authenticator tags */
+    public static final int AUTHENTICATOR_TAG = 0x62;
+    public static final int AUTHENTICATOR_AUTHENTICATOR_VNO_TAG = 0xA0;
+    public static final int AUTHENTICATOR_CREALM_TAG = 0xA1;
+    public static final int AUTHENTICATOR_CNAME_TAG = 0xA2;
+    public static final int AUTHENTICATOR_CKSUM_TAG = 0xA3;
+    public static final int AUTHENTICATOR_CUSEC_TAG = 0xA4;
+    public static final int AUTHENTICATOR_CTIME_TAG = 0xA5;
+    public static final int AUTHENTICATOR_SUBKEY_TAG = 0xA6;
+    public static final int AUTHENTICATOR_SEQ_NUMBER_TAG = 0xA7;
+    public static final int AUTHENTICATOR_AUTHORIZATION_DATA_TAG = 0xA8;
     
     /** AS-REQ's tags */
     public static final int AS_REQ_TAG = 0x6A;
@@ -105,6 +62,20 @@ public class KerberosConstants
     
     /** TGS-REP's tags */
     public static final int TGS_REP_TAG = 0x6D;
+    
+    /** AP-REQ tags */
+    public static final int AP_REQ_TAG = 0x6E;
+    public static final int AP_REQ_PVNO_TAG = 0xA0;
+    public static final int AP_REQ_MSG_TYPE_TAG = 0xA1;
+    public static final int AP_REQ_AP_OPTIONS_TAG = 0xA2;
+    public static final int AP_REQ_TICKET_TAG = 0xA3;
+    public static final int AP_REQ_AUTHENTICATOR_TAG = 0xA4;
+    
+    /** AP-REP tags */
+    public static final int AP_REP_TAG = 0x6F;
+    public static final int AP_REP_PVNO_TAG = 0xA0;
+    public static final int AP_REP_MSG_TYPE_TAG = 0xA1;
+    public static final int AP_REP_ENC_PART_TAG = 0xA2;
     
     /** KRB-ERROR tags */
     public static final int KRB_ERR_TAG = 0x7E;
@@ -121,18 +92,65 @@ public class KerberosConstants
     public static final int KRB_ERR_SNAME_TAG = 0xAA;
     public static final int KRB_ERR_ETEXT_TAG = 0xAB;
     public static final int KRB_ERR_EDATA_TAG = 0xAC;
+
+    //-------------------------------------------------------------------------
+    // Components
+    //-------------------------------------------------------------------------
+    /** AuthorizationData tags */
+    public static final int AUTHORIZATION_DATA_ADTYPE_TAG = 0xA0;
+    public static final int AUTHORIZATION_DATA_ADDATA_TAG = 0xA1;
+
+    /** Checksum tags */
+    public static final int CHECKSUM_TYPE_TAG = 0xA0;
+    public static final int CHECKSUM_CHECKSUM_TAG = 0xA1;
+
+    /** Encrypteddata's tags */
+    public static final int ENCRYPTED_DATA_ETYPE_TAG = 0xA0;
+    public static final int ENCRYPTED_DATA_KVNO_TAG = 0xA1;
+    public static final int ENCRYPTED_DATA_CIPHER_TAG = 0xA2;
     
-    /** AP-REQ tags */
-    public static final int AP_REQ_TAG = 0x6E;
-    public static final int AP_REQ_PVNO_TAG = 0xA0;
-    public static final int AP_REQ_MSG_TYPE_TAG = 0xA1;
-    public static final int AP_REQ_AP_OPTIONS_TAG = 0xA2;
-    public static final int AP_REQ_TICKET_TAG = 0xA3;
-    public static final int AP_REQ_AUTHENTICATOR_TAG = 0xA4;
+    /** EncryptionKey tags */
+    public static final int ENCRYPTION_KEY_TYPE_TAG = 0xA0;
+    public static final int ENCRYPTION_KEY_VALUE_TAG = 0xA1;
+
+    /** HostAddress' tags */
+    public static final int HOST_ADDRESS_ADDR_TYPE_TAG = 0xA0;
+    public static final int HOST_ADDRESS_ADDRESS_TAG = 0xA1;
     
-    /** AP-REP tags */
-    public static final int AP_REP_TAG = 0x6F;
-    public static final int AP_REP_PVNO_TAG = 0xA0;
-    public static final int AP_REP_MSG_TYPE_TAG = 0xA1;
-    public static final int AP_REP_ENC_PART_TAG = 0xA2;
+    /** KRB-REP's tags */
+    public static final int KDC_REP_PVNO_TAG = 0xA0;
+    public static final int KDC_REP_MSG_TYPE_TAG = 0xA1;
+    public static final int KDC_REP_PA_DATA_TAG = 0xA2;
+    public static final int KDC_REP_CREALM_TAG = 0xA3;
+    public static final int KDC_REP_CNAME_TAG = 0xA4;
+    public static final int KDC_REP_TICKET_TAG = 0xA5;
+    public static final int KDC_REP_ENC_PART_TAG = 0xA6;
+    
+    /** KRB-REQ's tags */
+    public static final int KDC_REQ_PVNO_TAG = 0xA1;
+    public static final int KDC_REQ_MSG_TYPE_TAG = 0xA2;
+    public static final int KDC_REQ_PA_DATA_TAG = 0xA3;
+    public static final int KDC_REQ_KDC_REQ_BODY_TAG = 0xA4;
+
+    /** KRB-REQ-BODY's tags */
+    public static final int KDC_REQ_BODY_KDC_OPTIONS_TAG = 0xA0;
+    public static final int KDC_REQ_BODY_CNAME_TAG = 0xA1;
+    public static final int KDC_REQ_BODY_REALM_TAG = 0xA2;
+    public static final int KDC_REQ_BODY_SNAME_TAG = 0xA3;
+    public static final int KDC_REQ_BODY_FROM_TAG = 0xA4;
+    public static final int KDC_REQ_BODY_TILL_TAG = 0xA5;
+    public static final int KDC_REQ_BODY_RTIME_TAG = 0xA6;
+    public static final int KDC_REQ_BODY_NONCE_TAG = 0xA7;
+    public static final int KDC_REQ_BODY_ETYPE_TAG = 0xA8;
+    public static final int KDC_REQ_BODY_ADDRESSES_TAG = 0xA9;
+    public static final int KDC_REQ_BODY_ENC_AUTHZ_DATA_TAG = 0xAA;
+    public static final int KDC_REQ_BODY_ADDITIONAL_TICKETS_TAG = 0xAB;
+
+    /** PaData tags */
+    public static final int PADATA_TYPE_TAG = 0xA1;
+    public static final int PADATA_VALUE_TAG = 0xA2;
+
+    /** PrincipalName's tags */
+    public static final int PRINCIPAL_NAME_NAME_TYPE_TAG = 0xA0;
+    public static final int PRINCIPAL_NAME_NAME_STRING_TAG = 0xA1;
 }
