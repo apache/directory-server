3f21d9fe0729589b5288bd8be2fa569509755807
Emmanuel Lecharny <elecharny@apache.org>
2010-12-29 19:08:52 +0000

    Created a empty test class for the lookup operation
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-AP@1053707 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-integ/src/test/java/org/apache/directory/server/core/subtree/SubentryLookupOperationIT.java
@@ -0,0 +1,47 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.directory.server.core.subtree;
+
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.integ.FrameworkRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Test cases for the AdministrativePoint interceptor lookup operation.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+@RunWith(FrameworkRunner.class)
+@CreateDS(name = "AdministrativePointServiceIT")
+public class SubentryLookupOperationIT
+{
+    // ===================================================================
+    // Test the Lookup operation on APs
+    // -------------------------------------------------------------------
+
+    // ===================================================================
+    // Test the Lookup operation on Subentries
+    // -------------------------------------------------------------------
+
+    // ===================================================================
+    // Test the Lookup operation on Entries
+    // -------------------------------------------------------------------
+
+}
