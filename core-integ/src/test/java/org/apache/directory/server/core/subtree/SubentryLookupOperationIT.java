2506295fed5355ad5e6222f15badb4357d7eede2
Emmanuel Lecharny <elecharny@apache.org>
2010-12-30 01:37:20 +0000

    Added some test descriptions
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-AP@1053782 13f79535-47bb-0310-9956-ffa450edef68

--- core-integ/src/test/java/org/apache/directory/server/core/subtree/SubentryLookupOperationIT.java
+++ core-integ/src/test/java/org/apache/directory/server/core/subtree/SubentryLookupOperationIT.java
@@ -21,6 +21,7 @@ package org.apache.directory.server.core.subtree;
 
 import org.apache.directory.server.core.annotations.CreateDS;
 import org.apache.directory.server.core.integ.FrameworkRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -35,13 +36,108 @@ public class SubentryLookupOperationIT
     // ===================================================================
     // Test the Lookup operation on APs
     // -------------------------------------------------------------------
+    /**
+     * Test the lookup of an AP. All APs are searcheable by default
+     */
+    @Test
+    public void testLookupAP() throws Exception
+    {
+        // TODO
+    }
 
+    
     // ===================================================================
     // Test the Lookup operation on Subentries
     // -------------------------------------------------------------------
+    /**
+     * Test the lookup of a subentry. Subentries can be read directly as it's 
+     * a OBJECT search.
+     */
+    @Test
+    public void testLookupSubentry() throws Exception
+    {
+        // TODO
+    }
 
+
+    /**
+     * Test the lookup of a subentry with the subentries control.
+     */
+    @Test
+    public void testLookupSubentryWithControl() throws Exception
+    {
+        // TODO
+    }
+    
+    
     // ===================================================================
     // Test the Lookup operation on Entries
     // -------------------------------------------------------------------
+    /**
+     * Test the lookup of a entry with no APs. The entry should not have
+     * any SN
+     */
+    @Test
+    public void testLookupEntryNoAp() throws Exception
+    {
+        // TODO
+    }
 
+
+    /**
+     * Test the lookup of a entry added under an AP with no subentry. All
+     * the entry SN must be set to -1, and not have any subentries reference
+     */
+    @Test
+    public void testLookupEntryUnderApNoSubentry() throws Exception
+    {
+        // TODO
+    }
+
+
+    /**
+     * Test the lookup of a entry when an AP with no subentry has been added. All
+     * the entry SN must be set to -1, and not have any subentries reference
+     */
+    @Test
+    public void testLookupEntryAfterApAdditionNoSubentry() throws Exception
+    {
+        // TODO
+    }
+
+
+    /**
+     * Test the lookup of a entry added under an AP with a subentry. 
+     * The entry is part of the subtreeSpecification.
+     * All the entry SN must be set to the AP SN, and have any subentries reference
+     */
+    @Test
+    public void testLookupEntryUnderApWithSubentrySelected() throws Exception
+    {
+        // TODO
+    }
+
+
+    /**
+     * Test the lookup of a entry when an AP with a subentry is added. 
+     * The entry is part of the subtreeSpecification.
+     * All the entry SN must be set to the AP SN, and have any subentries reference
+     */
+    @Test
+    public void testLookupEntryAfterApAdditionWithSubentrySelected() throws Exception
+    {
+        // TODO
+    }
+
+
+    /**
+     * Test the lookup of a entry when the subentry it referes has been 
+     * removed. The entry's reference to the subentry must be removed, the 
+     * SN must be the AP SN
+     */
+    @Test
+    public void testLookupEntryAfterSubentryDeletion() throws Exception
+    {
+        // TODO
+    }
 }
