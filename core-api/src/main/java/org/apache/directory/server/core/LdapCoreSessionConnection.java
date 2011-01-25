01eb041063304cdee56cbd9a1b8063755e2ed5d7
Alex Karasulu <akarasulu@apache.org>
2011-01-25 03:22:11 +0000

    added encoding decorator to decouple ModifyDnResponseImpl
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1063122 13f79535-47bb-0310-9956-ffa450edef68

--- core-api/src/main/java/org/apache/directory/server/core/LdapCoreSessionConnection.java
+++ core-api/src/main/java/org/apache/directory/server/core/LdapCoreSessionConnection.java
@@ -69,7 +69,7 @@ import org.apache.directory.shared.ldap.model.message.LdapResult;
 import org.apache.directory.shared.ldap.model.message.ModifyDnRequest;
 import org.apache.directory.shared.ldap.model.message.ModifyDnRequestImpl;
 import org.apache.directory.shared.ldap.model.message.ModifyDnResponse;
-import org.apache.directory.shared.ldap.message.ModifyDnResponseImpl;
+import org.apache.directory.shared.ldap.model.message.ModifyDnResponseImpl;
 import org.apache.directory.shared.ldap.model.message.ModifyRequest;
 import org.apache.directory.shared.ldap.message.ModifyRequestImpl;
 import org.apache.directory.shared.ldap.model.message.ModifyResponse;
