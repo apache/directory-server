269bd61eb50a21f8c161160fd8f49f70e308d88f
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2011-07-08 14:54:53 +0000

    Updated version in launch scripts.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1144347 13f79535-47bb-0310-9956-ffa450edef68

--- service/apacheds.sh
+++ service/apacheds.sh
@@ -18,7 +18,7 @@
 # under the License.
 
 # The final jar
-JAR=target/apacheds-service-2.0.0-M1.jar
+JAR=target/apacheds-service-2.0.0-M2-SNAPSHOT.jar
 
 # Checking if the final jar exists
 if [ -e $JAR ] ; then
