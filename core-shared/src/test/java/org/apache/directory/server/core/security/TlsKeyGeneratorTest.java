dbef7a298fc5ad39f9ca0d2f761ca66f29e2ac99
Selcuk Aya <saya@apache.org>
2011-11-02 12:06:16 +0000

    first part of package move to core-shared
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-txns@1196552 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-shared/src/test/java/org/apache/directory/server/core/security/TlsKeyGeneratorTest.java
@@ -0,0 +1,102 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+package org.apache.directory.server.core.security;
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
+
+import org.apache.directory.shared.ldap.model.constants.SchemaConstants;
+import org.apache.directory.shared.ldap.model.entry.DefaultEntry;
+import org.apache.directory.shared.ldap.model.entry.Entry;
+import org.apache.directory.shared.ldap.model.name.Dn;
+import org.apache.directory.shared.ldap.model.schema.SchemaManager;
+import org.apache.directory.shared.ldap.schemaloader.LdifSchemaLoader;
+import org.apache.directory.shared.ldap.schemamanager.impl.DefaultSchemaManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mycila.junit.concurrent.Concurrency;
+import com.mycila.junit.concurrent.ConcurrentJunitRunner;
+
+
+/**
+ * Test for the TlsKeyGenerator class.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+@RunWith(ConcurrentJunitRunner.class)
+@Concurrency()
+public class TlsKeyGeneratorTest
+{
+    private static final Logger LOG = LoggerFactory.getLogger( TlsKeyGeneratorTest.class );
+    private static LdifSchemaLoader loader;
+    private static SchemaManager schemaManager;
+
+
+    /**
+     * Initialize the registries once for the whole test suite
+     */
+    @BeforeClass
+    public static void setup() throws Exception
+    {
+        /*
+        String workingDirectory = System.getProperty( "workingDirectory" );
+
+        if ( workingDirectory == null )
+        {
+            String path = TlsKeyGeneratorTest.class.getResource( "" ).getPath();
+            int targetPos = path.indexOf( "target" );
+            workingDirectory = path.substring( 0, targetPos + 6 );
+        }
+
+        File schemaRepository = new File( workingDirectory, "schema" );
+        SchemaLdifExtractor extractor = new DefaultSchemaLdifExtractor( new File( workingDirectory ) );
+        extractor.extractOrCopy( true );
+        */
+        
+        schemaManager = new DefaultSchemaManager();
+    }
+
+
+    /**
+     * Test method for all methods in one.
+     */
+    @Test
+    public void testAll() throws Exception
+    {
+        Entry entry = new DefaultEntry( schemaManager, new Dn( schemaManager ) );
+        TlsKeyGenerator.addKeyPair( entry );
+        LOG.debug( "Entry: {}", entry );
+        assertTrue( entry.contains( SchemaConstants.OBJECT_CLASS_AT, TlsKeyGenerator.TLS_KEY_INFO_OC ) );
+
+        KeyPair keyPair = TlsKeyGenerator.getKeyPair( entry );
+        assertNotNull( keyPair );
+
+        X509Certificate cert = TlsKeyGenerator.getCertificate( entry );
+        assertNotNull( cert );
+    }
+}
