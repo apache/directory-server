02cff3962587222cdc68c386afa444e4db7895ed
Emmanuel Lecharny <elecharny@apache.org>
2012-01-03 11:18:51 +0000

    Fixed a failing test
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/branches/apacheds-txns@1226741 13f79535-47bb-0310-9956-ffa450edef68

--- server-integ/src/test/java/org/apache/directory/server/operations/add/AddIT.java
+++ server-integ/src/test/java/org/apache/directory/server/operations/add/AddIT.java
@@ -51,6 +51,8 @@ import javax.naming.directory.SearchResult;
 import javax.naming.ldap.LdapContext;
 import javax.naming.ldap.LdapName;
 
+import netscape.ldap.LDAPException;
+
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 import org.apache.directory.server.annotations.CreateLdapServer;
@@ -115,10 +117,10 @@ import org.slf4j.LoggerFactory;
                     "objectClass: top\n" +
                     "objectClass: domain\n\n"),
             indexes =
-            {
-                @CreateIndex(attribute = "objectClass"),
-                @CreateIndex(attribute = "dc"),
-                @CreateIndex(attribute = "ou")
+                {
+                    @CreateIndex(attribute = "objectClass"),
+                    @CreateIndex(attribute = "dc"),
+                    @CreateIndex(attribute = "ou")
             }),
 
         @CreatePartition(
@@ -130,13 +132,13 @@ import org.slf4j.LoggerFactory;
                     "objectClass: top\n" +
                     "objectClass: domain\n\n"),
             indexes =
-            {
-                @CreateIndex(attribute = "objectClass"),
-                @CreateIndex(attribute = "dc"),
-                @CreateIndex(attribute = "ou")
+                {
+                    @CreateIndex(attribute = "objectClass"),
+                    @CreateIndex(attribute = "dc"),
+                    @CreateIndex(attribute = "ou")
             }) })
-@CreateLdapServer( name = "ADDIT",  transports =
-    { @CreateTransport(protocol = "LDAP", port = -1 ) })
+@CreateLdapServer(name = "ADDIT", transports =
+    { @CreateTransport(protocol = "LDAP", port = -1) })
 @ApplyLdifs(
     {
         // Entry # 0
@@ -195,7 +197,7 @@ public class AddIT extends AbstractLdapTestUnit
 
         // modify object classes, add two more
         Attributes attributes = LdifUtils.createJndiAttributes( "objectClass: organizationalPerson",
-                "objectClass: inetOrgPerson" );
+            "objectClass: inetOrgPerson" );
 
         DirContext person = ( DirContext ) ctx.lookup( RDN );
         person.modifyAttributes( "", DirContext.ADD_ATTRIBUTE, attributes );
@@ -704,16 +706,17 @@ public class AddIT extends AbstractLdapTestUnit
         addRequest.addControl( manageDSAIT );
 
         // add success
-        Entry entry = new DefaultEntry( "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system", 
+        Entry entry = new DefaultEntry( "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system",
             "objectClass", "organizationalUnit",
             "ou", "UnderReferral" );
-        
+
         addRequest.setEntry( entry );
 
         AddResponse addResponse = conn.add( addRequest );
         assertEquals( ResultCodeEnum.REFERRAL, addResponse.getLdapResult().getResultCode() );
 
-        assertNull( conn.lookup( "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system", new Control[]{ manageDSAIT } ) );
+        assertNull( conn.lookup( "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system", new Control[]
+            { manageDSAIT } ) );
 
         conn.close();
     }
@@ -783,8 +786,8 @@ public class AddIT extends AbstractLdapTestUnit
         AddRequest addRequest = new AddRequestImpl();
 
         // referrals failure
-        Entry entry = new DefaultEntry( 
-            "ou=UnderReferral,ou=Computers,uid=akarasuluref,ou=users,ou=system", 
+        Entry entry = new DefaultEntry(
+            "ou=UnderReferral,ou=Computers,uid=akarasuluref,ou=users,ou=system",
             "objectClass", "organizationalUnit",
             "ou", "UnderReferral" );
         addRequest.setEntry( entry );
@@ -793,9 +796,12 @@ public class AddIT extends AbstractLdapTestUnit
 
         assertEquals( ResultCodeEnum.REFERRAL, addResponse.getLdapResult().getResultCode() );
 
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://localhost:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://foo:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://bar:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://localhost:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://foo:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://bar:10389/ou=UnderReferral,ou=Computers,uid=akarasulu,ou=users,ou=system" ) );
 
         conn.close();
     }
@@ -817,20 +823,23 @@ public class AddIT extends AbstractLdapTestUnit
         //addRequest.addControl( manageDSAIT );
 
         // referrals failure
-        Entry entry = new DefaultEntry( 
-            "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system", 
+        Entry entry = new DefaultEntry(
+            "ou=UnderReferral,uid=akarasuluref,ou=users,ou=system",
             "objectClass", "organizationalUnit",
             "ou", "UnderReferral" );
-        
+
         addRequest.setEntry( entry );
 
         AddResponse addResponse = conn.add( addRequest );
 
         assertEquals( ResultCodeEnum.REFERRAL, addResponse.getLdapResult().getResultCode() );
 
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://localhost:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://foo:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
-        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls().contains( "ldap://bar:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://localhost:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://foo:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
+        assertTrue( addResponse.getLdapResult().getReferral().getLdapUrls()
+            .contains( "ldap://bar:10389/ou=UnderReferral,uid=akarasulu,ou=users,ou=system" ) );
 
         conn.close();
     }
@@ -886,7 +895,7 @@ public class AddIT extends AbstractLdapTestUnit
         attrs.put( "cn", "Jim, Bean" );
 
         DirContext jimBeanCtx = ctx.createSubcontext( "cn=\"Jim, Bean\"", attrs );
-        
+
         assertNotNull( jimBeanCtx );
     }
 
@@ -980,7 +989,7 @@ public class AddIT extends AbstractLdapTestUnit
      * Create an entry a Rdn which is not present in the entry,
      * with another attribute's value, and on a SingleValued attribute
      */
-    @Test
+    @Test(expected = InvalidAttributeValueException.class)
     public void testAddEntryDifferentRDNSingleValuedInEntry() throws Exception
     {
         DirContext ctx = ( DirContext ) getWiredContext( getLdapServer() ).lookup( BASE );
@@ -994,27 +1003,7 @@ public class AddIT extends AbstractLdapTestUnit
         person.put( "sn", "Michael Jackson" );
         person.put( "cn", "Jackson" );
 
-        DirContext michaelCtx = ctx.createSubcontext( "displayName=test", person );
-
-        assertNotNull( michaelCtx );
-
-        DirContext jackson = ( DirContext ) ctx.lookup( "displayName=test" );
-        person = jackson.getAttributes( "" );
-        javax.naming.directory.Attribute newOcls = person.get( "objectClass" );
-
-        String[] expectedOcls =
-            { "top", "person", "organizationalPerson", "inetOrgPerson" };
-
-        for ( String name : expectedOcls )
-        {
-            assertTrue( "object class " + name + " is present", newOcls.contains( name ) );
-        }
-
-        // Check that the displayName attribute has been replaced
-        javax.naming.directory.Attribute displayName = person.get( "displayName" );
-
-        assertEquals( 1, displayName.size() );
-        assertTrue( displayName.contains( "test" ) );
+        ctx.createSubcontext( "displayName=test", person );
     }
 
 
@@ -1329,10 +1318,10 @@ public class AddIT extends AbstractLdapTestUnit
      * @throws NamingException 
      */
     @Test
-    @CreateDS( 
+    @CreateDS(
         enableChangeLog = false,
-        name = "DSAlias" )
-    @CreateLdapServer( name = "DSAlias", transports =
+        name = "DSAlias")
+    @CreateLdapServer(name = "DSAlias", transports =
         { @CreateTransport(protocol = "LDAP", port = -1) })
     public void test_DIRSERVER_1357() throws Exception
     {
@@ -1367,6 +1356,7 @@ public class AddIT extends AbstractLdapTestUnit
         ctx.destroySubcontext( "cn=alias,ou=engineering" );
     }
 
+
     /**
      * Adding an entry with a non existing attribute type.
      * 
@@ -1389,9 +1379,9 @@ public class AddIT extends AbstractLdapTestUnit
         try
         {
             connection.add( personEntry );
-            fail("should throw LdapNoSuchAttributeException");
+            fail( "should throw LdapNoSuchAttributeException" );
         }
-        catch( LdapNoSuchAttributeException e )
+        catch ( LdapNoSuchAttributeException e )
         {
             //expected exception
         }
@@ -1408,7 +1398,7 @@ public class AddIT extends AbstractLdapTestUnit
      * 
      * @throws Exception 
      */
-    @Test( expected = LdapOperationException.class )
+    @Test(expected = LdapOperationException.class)
     public void testAddEntryNonExistingOC() throws Exception
     {
         LdapConnection connection = getAdminConnection( getLdapServer() );
@@ -1430,41 +1420,41 @@ public class AddIT extends AbstractLdapTestUnit
      * 
      * @throws Exception 
      */
-    @Test( expected = LdapException.class )
+    @Test(expected = LdapException.class)
     public void testAddEntry100KData() throws Exception
     {
         LdapConnection connection = getAdminConnection( getLdapServer() );
 
-        int size = 100*1024;
+        int size = 100 * 1024;
         byte[] dataBytes = new byte[size];
-        
-        for ( int i = 0; i < size; i++)
+
+        for ( int i = 0; i < size; i++ )
         {
             dataBytes[i] = 'A';
         }
-        
+
         String data = Strings.utf8ToString( dataBytes );
 
         Dn dn = new Dn( "cn=Kate Bush," + BASE );
-        
+
         Entry personEntry = new DefaultEntry( "cn=Kate Bush," + BASE,
-        "objectClass: top",
-        "objectClass: person",
-        "cn: Kate Bush",
-        "sn: Bush",
-        "description", data );
+            "objectClass: top",
+            "objectClass: person",
+            "cn: Kate Bush",
+            "sn: Bush",
+            "description", data );
 
         connection.add( personEntry );
-        
+
         // Check that the entry has been stored 
         Entry entry = connection.lookup( dn, "description", "cn", "sn" );
-        
+
         String description = entry.get( "description" ).getString();
-        
+
         assertNotNull( description );
         assertTrue( description.startsWith( "AAA" ) );
         assertEquals( size, description.length() );
-        
+
         for ( int i = 0; i < size; i++ )
         {
             assertEquals( 'A', description.charAt( i ) );
