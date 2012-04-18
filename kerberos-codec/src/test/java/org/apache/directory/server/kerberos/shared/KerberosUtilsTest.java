ab6f94cca8d4a0b65c69b6eff486bcc2106f4cb2
Emmanuel Lecharny <elecharny@apache.org>
2012-04-18 14:16:15 +0000

    Fixed issues with JDK 7, as the KerberosPrincpal class is now more strict.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1327517 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-codec/src/test/java/org/apache/directory/server/kerberos/shared/KerberosUtilsTest.java
+++ kerberos-codec/src/test/java/org/apache/directory/server/kerberos/shared/KerberosUtilsTest.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ * 
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ * 
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ * 
  */
 package org.apache.directory.server.kerberos.shared;
 
@@ -55,7 +55,7 @@ public class KerberosUtilsTest
     @Test
     public void testKerberosNameSimple() throws Exception
     {
-        KerberosPrincipal kp = new KerberosPrincipal( "abc" );
+        KerberosPrincipal kp = new KerberosPrincipal( "abc@APACHE.ORG" );
         List<String> names = KerberosUtils.getNames( kp );
 
         assertEquals( 1, names.size() );
@@ -90,7 +90,7 @@ public class KerberosUtilsTest
     @Test
     public void testKerberosNameThree() throws Exception
     {
-        KerberosPrincipal kp = new KerberosPrincipal( "abc/def/ghi" );
+        KerberosPrincipal kp = new KerberosPrincipal( "abc/def/ghi@APACHE.ORG" );
         List<String> names = KerberosUtils.getNames( kp );
 
         assertEquals( 3, names.size() );
