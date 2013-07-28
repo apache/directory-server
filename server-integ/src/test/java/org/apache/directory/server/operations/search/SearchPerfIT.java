fa5561b5f525a82c70f36dfd2852dc1a9b2be4ca
Kiran Ayyagari <kayyagari@apache.org>
2013-07-28 11:04:15 +0000

    print the scope along with the numbers
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1507778 13f79535-47bb-0310-9956-ffa450edef68

--- server-integ/src/test/java/org/apache/directory/server/operations/search/SearchPerfIT.java
+++ server-integ/src/test/java/org/apache/directory/server/operations/search/SearchPerfIT.java
@@ -159,8 +159,8 @@ public class SearchPerfIT extends AbstractLdapTestUnit
             long t1 = System.currentTimeMillis();
 
             Long deltaWarmed = ( t1 - t00 );
-            System.out.println( "Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed )
-                + " per s ) /" + ( t1 - t0 ) + " , count : " + count );
+            System.out.println( "OBJECT level - Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed )
+                + " per s ) /" + ( t1 - t0 ) + ", count : " + count );
 
             System.out.println( "DeltaSearch : " + ( deltaSearch / nbIterations ) );
             System.out.println( "DeltaGet : " + ( deltaGet / nbIterations ) );
@@ -265,8 +265,7 @@ public class SearchPerfIT extends AbstractLdapTestUnit
             long t1 = System.currentTimeMillis();
 
             Long deltaWarmed = ( t1 - t00 );
-            System.out.println( "Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed )
-                * 5
+            System.out.println( "ONE level - Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed ) * 5
                 + " per s ) /" + ( t1 - t0 ) + ", count : " + count );
         }
         catch ( LdapException e )
@@ -369,7 +368,7 @@ public class SearchPerfIT extends AbstractLdapTestUnit
             long t1 = System.currentTimeMillis();
 
             Long deltaWarmed = ( t1 - t00 );
-            System.out.println( "Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed )
+            System.out.println( "SUB level - Delta : " + deltaWarmed + "( " + ( ( ( nbIterations - 50000 ) * 1000 ) / deltaWarmed )
                 * 10
                 + " per s ) /" + ( t1 - t0 ) + ", count : " + count );
         }
