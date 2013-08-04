dbe5e06e51698d43e8a8fa0eed5b6c3e9da225e4
Kiran Ayyagari <kayyagari@apache.org>
2013-08-04 09:37:39 +0000

    o partition factory for mavibot backend o enabled mavibot partition (now mavibot snapshots are available in maven repo)
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1510118 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-annotations/src/main/java/org/apache/directory/server/core/factory/MavibotPartitionFactory.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.directory.server.core.factory;
+
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.name.Dn;
+import org.apache.directory.api.ldap.model.schema.SchemaManager;
+import org.apache.directory.server.core.api.partition.Partition;
+import org.apache.directory.server.core.partition.impl.btree.mavibot.MavibotIndex;
+import org.apache.directory.server.core.partition.impl.btree.mavibot.MavibotPartition;
+import org.apache.directory.server.xdbm.Index;
+import org.apache.directory.mavibot.btree.RecordManager;
+
+
+/**
+ * A factory used to generate {@link MavibotPartition}s.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public class MavibotPartitionFactory implements PartitionFactory
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public MavibotPartition createPartition( SchemaManager schemaManager, String id, String suffix, int cacheSize,
+        File workingDirectory )
+        throws Exception
+    {
+        MavibotPartition partition = new MavibotPartition( schemaManager );
+        partition.setId( id );
+        partition.setSuffixDn( new Dn( suffix ) );
+        partition.setCacheSize( 500 );
+        partition.setPartitionPath( workingDirectory.toURI() );
+
+        return partition;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public void addIndex( Partition partition, String attributeId, int cacheSize ) throws Exception
+    {
+        if ( !( partition instanceof MavibotPartition ) )
+        {
+            throw new IllegalArgumentException( "Partition must be a MavibotPartition" );
+        }
+
+        MavibotPartition mavibotPartition = ( MavibotPartition ) partition;
+        Set<Index<?, ?, String>> indexedAttributes = mavibotPartition.getIndexedAttributes();
+
+        RecordManager recordMan = ( ( MavibotPartition ) partition ).getRecordMan();
+        
+        MavibotIndex<Object, Entry> index = new MavibotIndex<Object, Entry>( recordMan, attributeId, false );
+        index.setCacheSize( cacheSize );
+
+        indexedAttributes.add( index );
+        mavibotPartition.setIndexedAttributes( indexedAttributes );
+    }
+
+}
