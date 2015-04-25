17b3d130644236e38d3bd937841a5f23a30f6f65
Stefan Seelmann <seelmann@apache.org>
2015-04-25 19:46:14 +0000

    Add tests for zip and tar.gz archive (DIRSERVER-2055, DIRSERVER-2065, DIRSERVER-2063)
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1676060 13f79535-47bb-0310-9956-ffa450edef68

--- installers/src/test/docker/run-tests.sh
+++ installers/src/test/docker/run-tests.sh
@@ -19,48 +19,87 @@
 
 set -e
 
-# Debian installer 64bit
+# Debian package 64bit
 DEB64="${project.build.directory}/installers/apacheds-${project.version}-amd64.deb"
 if [ -f ${DEB64} ]
 then
-    # Run deb installer with official Java image (Debian 8, OpenJDK 8, 64bit):
+    echo
+    echo
+    echo "Testing debian package with official Java image (Debian 8, OpenJDK 8, 64bit)"
     docker run -i --rm \
       -v ${DEB64}:/apacheds.deb \
       -v ${project.build.directory}/docker/deb.test:/deb.test \
       java:8 bash /deb.test
 
-    # Run deb installer with 'dockerfile' Java image (Ubuntu 14.04, Oracle Java 7, 64bit):
+    echo
+    echo
+    echo "Testing debian package with 'dockerfile' Java image (Ubuntu 14.04, Oracle Java 7, 64bit)"
     docker run -i --rm \
       -v ${DEB64}:/apacheds.deb \
       -v ${project.build.directory}/docker/deb.test:/deb.test \
       dockerfile/java:oracle-java7 bash /deb.test
 fi
 
-# Binary Installer 64bit
-BIN64="${project.build.directory}/installers/apacheds-${project.version}-64bit.bin"
-if [ -f ${BIN64} ]
-then
-    # Run bin installer with official Java image (Debian 8, OpenJDK 8, 64bit):
-    docker run -i --rm \
-      -v ${BIN64}:/apacheds.bin \
-      -v ${project.build.directory}/docker/bin.test:/bin.test \
-      java:8 bash /bin.test
-fi
 
-# RPM installer 64bit
+# RPM package 64bit
 RPM64="${project.build.directory}/installers/apacheds-${project.version}-x86_64.rpm"
 if [ -f ${RPM64} ]
 then
-    # Run rpm installer with official Fedora (Fedora 21, OpenJDK 8)
-    #docker run -i --rm \
-    #  -v ${RPM64}:/apacheds.rpm \
-    #  -v ${project.build.directory}/docker/rpm.test:/rpm.test \
-    #  fedora:latest bash /rpm.test
+    echo
+    echo
+    echo "Testing RPM package with official Fedora image (Fedora 21, OpenJDK 8)"
+    docker run -i --rm \
+      -v ${RPM64}:/apacheds.rpm \
+      -v ${project.build.directory}/docker/rpm.test:/rpm.test \
+      fedora:21 bash /rpm.test
 
-    # Run rpm installer with official Centos (CentOS 7, OpenJDK 8)
+    echo
+    echo
+    echo "Testing RPM package with official Centos image (CentOS 7, OpenJDK 8, 64bit)"
     docker run -i --rm \
       -v ${RPM64}:/apacheds.rpm \
       -v ${project.build.directory}/docker/rpm.test:/rpm.test \
       centos:7 bash /rpm.test
 fi
 
+
+# Binalry installer 64bit
+BIN64="${project.build.directory}/installers/apacheds-${project.version}-64bit.bin"
+if [ -f ${BIN64} ]
+then
+    echo
+    echo
+    echo "Testing bin installer with official Java image (Debian 8, OpenJDK 8, 64bit)"
+    docker run -i --rm \
+      -v ${BIN64}:/apacheds.bin \
+      -v ${project.build.directory}/docker/bin.test:/bin.test \
+      java:8 bash /bin.test
+fi
+
+
+# tar.gz archive
+TGZ="${project.build.directory}/installers/apacheds-${project.version}.tar.gz"
+if [ -f ${TGZ} ]
+then
+    echo
+    echo
+    echo "Testing tar.gz archive with official Java image (Debian 8, OpenJDK 8, 64bit)"
+    docker run -it --rm \
+      -v ${TGZ}:/apacheds.tar.gz \
+      -v ${project.build.directory}/docker/archive.test:/archive.test \
+      java:8 bash /archive.test
+fi
+
+
+# zip archive
+ZIP="${project.build.directory}/installers/apacheds-${project.version}.zip"
+if [ -f ${ZIP} ]
+then
+    echo
+    echo
+    echo "Testing zip archive with 'dockerfile' Java image (Ubuntu 14.04, Oracle Java 7, 64bit)"
+    docker run -it --rm \
+      -v ${ZIP}:/apacheds.zip \
+      -v ${project.build.directory}/docker/archive.test:/archive.test \
+      dockerfile/java:oracle-java7 bash /archive.test
+fi
