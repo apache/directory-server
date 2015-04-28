e7494b7b4dbe60828a874e29314a572398d22b5d
Emmanuel Lecharny <elecharny@apache.org>
2015-04-28 19:42:25 +0000

    Added the missing AL 2.0 header and a class header
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1676600 13f79535-47bb-0310-9956-ffa450edef68

--- ldap-client-test/src/test/java/org/apache/directory/shared/client/api/TrackingLdapConnectionFactory.java
+++ ldap-client-test/src/test/java/org/apache/directory/shared/client/api/TrackingLdapConnectionFactory.java
@@ -1,17 +1,47 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
 package org.apache.directory.shared.client.api;
+
 
 import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.ldap.client.api.DefaultLdapConnectionFactory;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapConnectionConfig;
 
-public class TrackingLdapConnectionFactory extends DefaultLdapConnectionFactory {
+
+/**
+ * A class used to count the number of binds done
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+public class TrackingLdapConnectionFactory extends DefaultLdapConnectionFactory
+{
     private int bindCalled = 0;
-    
-    public TrackingLdapConnectionFactory( LdapConnectionConfig config ) {
+
+
+    public TrackingLdapConnectionFactory( LdapConnectionConfig config )
+    {
         super( config );
     }
-    
+
+
     @Override
     public LdapConnection bindConnection( LdapConnection connection ) throws LdapException
     {
@@ -19,13 +49,16 @@ public class TrackingLdapConnectionFactory extends DefaultLdapConnectionFactory 
         return super.bindConnection( connection );
     }
 
+
     @Override
     public LdapConnection configureConnection( LdapConnection connection )
     {
         return super.configureConnection( connection );
     }
-    
-    public int getBindCalled() {
+
+
+    public int getBindCalled()
+    {
         return bindCalled;
     }
 }
