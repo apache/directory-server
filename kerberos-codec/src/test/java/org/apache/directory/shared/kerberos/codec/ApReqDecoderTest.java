51edaeef23202e3de0a4711fc8e87e6d523cd463
Emmanuel Lecharny <elecharny@apache.org>
2015-04-28 18:21:55 +0000

    Added a perf test.
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1676586 13f79535-47bb-0310-9956-ffa450edef68

--- kerberos-codec/src/test/java/org/apache/directory/shared/kerberos/codec/ApReqDecoderTest.java
+++ kerberos-codec/src/test/java/org/apache/directory/shared/kerberos/codec/ApReqDecoderTest.java
@@ -26,17 +26,18 @@ import static org.junit.Assert.fail;
 
 import java.nio.ByteBuffer;
 
-import com.mycila.junit.concurrent.Concurrency;
-import com.mycila.junit.concurrent.ConcurrentJunitRunner;
-
 import org.apache.directory.api.asn1.DecoderException;
 import org.apache.directory.api.asn1.EncoderException;
 import org.apache.directory.api.asn1.ber.Asn1Container;
 import org.apache.directory.api.asn1.ber.Asn1Decoder;
 import org.apache.directory.shared.kerberos.codec.apReq.ApReqContainer;
 import org.apache.directory.shared.kerberos.messages.ApReq;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.mycila.junit.concurrent.Concurrency;
+import com.mycila.junit.concurrent.ConcurrentJunitRunner;
 
 
 /**
@@ -359,5 +360,141 @@ public class ApReqDecoderTest
         // Decode the AP-REQ PDU
         kerberosDecoder.decode( stream, apReqContainer );
         fail();
+    }
+
+
+    @Test
+    @Ignore
+    public void testDecodeFullApReqPerf() throws Exception
+    {
+        ByteBuffer stream = ByteBuffer.allocate( 0x6C );
+
+        stream.put( new byte[]
+            {
+                0x6E, 0x6A,
+                0x30, 0x68,
+                ( byte ) 0xA0, 0x03, // pvno
+                0x02,
+                0x01,
+                0x05,
+                ( byte ) 0xA1,
+                0x03, // msg-type
+                0x02,
+                0x01,
+                0x0E,
+                ( byte ) 0xA2,
+                0x07, // APOptions
+                0x03,
+                0x05,
+                0x00,
+                0x60,
+                0x00,
+                0x00,
+                0x00,
+                ( byte ) 0xA3,
+                0x40, // Ticket
+                0x61,
+                0x3E,
+                0x30,
+                0x3C,
+                ( byte ) 0xA0,
+                0x03,
+                0x02,
+                0x01,
+                0x05,
+                ( byte ) 0xA1,
+                0x0D,
+                0x1B,
+                0x0B,
+                'E',
+                'X',
+                'A',
+                'M',
+                'P',
+                'L',
+                'E',
+                '.',
+                'C',
+                'O',
+                'M',
+                ( byte ) 0xA2,
+                0x13,
+                0x30,
+                0x11,
+                ( byte ) 0xA0,
+                0x03,
+                0x02,
+                0x01,
+                0x01,
+                ( byte ) 0xA1,
+                0x0A,
+                0x30,
+                0x08,
+                0x1B,
+                0x06,
+                'c',
+                'l',
+                'i',
+                'e',
+                'n',
+                't',
+                ( byte ) 0xA3,
+                0x11,
+                0x30,
+                0x0F,
+                ( byte ) 0xA0,
+                0x03,
+                0x02,
+                0x01,
+                0x11,
+                ( byte ) 0xA2,
+                0x08,
+                0x04,
+                0x06,
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                ( byte ) 0xA4,
+                0x11, // Authenticator
+                0x30,
+                0x0F,
+                ( byte ) 0xA0,
+                0x03,
+                0x02,
+                0x01,
+                0x11,
+                ( byte ) 0xA2,
+                0x08,
+                0x04,
+                0x06,
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+        } );
+
+        stream.flip();
+
+        // Allocate a ApReq Container
+        //ApReqContainer apReqContainer = new ApReqContainer( stream );
+        Asn1Decoder kerberosDecoder = new Asn1Decoder();
+        ApReqContainer apReqContainer = new ApReqContainer( stream );
+
+        long start = System.currentTimeMillis();
+
+        for ( int i = 0; i < 10000000; ++i )
+        {
+            kerberosDecoder.decode( stream, apReqContainer );
+            //String serverName = apreqContainer.getApReq().getTicket().getSName().toString();
+            stream.rewind();
+            apReqContainer.clean();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println( "ApacheDS takes:" + ( end - start ) );
     }
 }
