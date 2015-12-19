66ec0bfda6b307e5e78eb12dae2b0d3d03a1eb0b
Stefan Seelmann <seelmann@apache.org>
2015-12-19 12:34:53 +0000

    Test for DIRSERVER-2109
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1720906 13f79535-47bb-0310-9956-ffa450edef68

--- test-framework/src/test/java/org/apache/directory/server/core/integ/TestClassServer.java
+++ test-framework/src/test/java/org/apache/directory/server/core/integ/TestClassServer.java
@@ -19,8 +19,10 @@
 package org.apache.directory.server.core.integ;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.server.core.annotations.ApplyLdifFiles;
 import org.junit.Test;
@@ -37,5 +39,11 @@ public class TestClassServer extends AbstractLdapTestUnit
         assertTrue( getService().getAdminSession().exists( new Dn( "cn=testPerson1,ou=system" ) ) );
 
         assertTrue( getService().getAdminSession().exists( new Dn( "cn=testPerson2,ou=system" ) ) );
+
+        assertTrue( getService().getAdminSession().exists( new Dn( "cn=\\#\\\\\\+\\, \\\"\u00F6\u00E9\\\",ou=users,ou=system" ) ) );
+        Entry entry = getService().getAdminSession().lookup( new Dn("cn=\\#\\\\\\+\\, \\\"\u00F6\u00E9\\\",ou=users,ou=system") );
+        System.out.println( entry );
+        assertEquals(1, entry.get( "cn" ).size());
+        assertEquals("#\\+, \"\u00F6\u00E9\"", entry.get( "cn" ).get().getString());
     }
 }
