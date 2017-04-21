cce59a752c30dc3a747773d053bd97b5e4532079
Emmanuel Lécharny <elecharny@apache.org>
2017-04-21 11:39:17 +0000

    Check that the ads_pwdValidator is not null, and if so, use the DefaultPasswordValidator class
    git-svn-id: https://svn.apache.org/repos/asf/directory/apacheds/trunk@1792187 13f79535-47bb-0310-9956-ffa450edef68

--- interceptors/authn/src/main/java/org/apache/directory/server/core/authn/AuthenticationInterceptor.java
+++ interceptors/authn/src/main/java/org/apache/directory/server/core/authn/AuthenticationInterceptor.java
@@ -78,8 +78,10 @@ import org.apache.directory.server.core.api.DirectoryService;
 import org.apache.directory.server.core.api.InterceptorEnum;
 import org.apache.directory.server.core.api.LdapPrincipal;
 import org.apache.directory.server.core.api.authn.ppolicy.CheckQualityEnum;
+import org.apache.directory.server.core.api.authn.ppolicy.DefaultPasswordValidator;
 import org.apache.directory.server.core.api.authn.ppolicy.PasswordPolicyConfiguration;
 import org.apache.directory.server.core.api.authn.ppolicy.PasswordPolicyException;
+import org.apache.directory.server.core.api.authn.ppolicy.PasswordValidator;
 import org.apache.directory.server.core.api.filtering.EntryFilteringCursor;
 import org.apache.directory.server.core.api.interceptor.BaseInterceptor;
 import org.apache.directory.server.core.api.interceptor.context.AddOperationContext;
@@ -1433,7 +1435,15 @@ public class AuthenticationInterceptor extends BaseInterceptor
         // perform the length validation
         validatePasswordLength( strPassword, policyConfig );
 
-        policyConfig.getPwdValidator().validate( strPassword, entry );
+        PasswordValidator passwordValidator = policyConfig.getPwdValidator();
+        
+        if ( passwordValidator == null )
+        {
+            // Use the default one
+            passwordValidator = new DefaultPasswordValidator();
+        }
+        
+        passwordValidator.validate( strPassword, entry );
     }
 
 
