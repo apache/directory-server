472afd50b3b53e27ab658f51776304495045e32e
Stefan Seelmann <mail@stefan-seelmann.de>
2019-04-20 23:02:25 +0200

    Implement Jenkins pipeline and update installer tests to recent OS and Java version

---
+++ installers/src/test/docker/run-archive-tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License. 
+
+set -e
+
+TEST_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALLERS_DIR="$TEST_SCRIPTS_DIR/../installers"
+
+# tar.gz archive
+TGZ="${INSTALLERS_DIR}/apacheds-${project.version}.tar.gz"
+if [ -f ${TGZ} ]
+then
+    echo
+    echo
+    echo "Testing tar.gz archive (Debian 9, OpenJDK 8, 64bit)"
+    docker run -i --rm \
+      -v ${TGZ}:/apacheds.tar.gz \
+      -v ${TEST_SCRIPTS_DIR}/archive.test:/archive.test \
+      openjdk:8 bash /archive.test
+fi
+
+
+# zip archive
+ZIP="${INSTALLERS_DIR}/apacheds-${project.version}.zip"
+if [ -f ${ZIP} ]
+then
+    echo
+    echo
+    #echo "Testing zip archive (Debian 9, OpenJDK 11, 64bit)"
+    docker run -i --rm \
+      -v ${ZIP}:/apacheds.zip \
+      -v ${TEST_SCRIPTS_DIR}/archive.test:/archive.test \
+      openjdk:11 bash /archive.test
+fi
---
+++ installers/src/test/docker/run-rpm-tests.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License. 
+
+set -e
+
+TEST_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALLERS_DIR="$TEST_SCRIPTS_DIR/../installers"
+
+# RPM package 64bit
+RPM64="${INSTALLERS_DIR}/apacheds-${project.version}-x86_64.rpm"
+if [ -f ${RPM64} ]
+then
+    echo
+    echo
+    echo "Testing rpm package (Centos 7, OpenJDK 8, 64bit)"
+    docker run -i --rm \
+      -v ${RPM64}:/apacheds.rpm \
+      -v ${TEST_SCRIPTS_DIR}/rpm.test:/rpm.test \
+      centos:7 bash /rpm.test
+
+    echo
+    echo
+    echo "Testing rpm package (Fedora 29, OpenJDK 12, 64bit)"
+    docker run -i --rm \
+      -v ${RPM64}:/apacheds.rpm \
+      -v ${TEST_SCRIPTS_DIR}/rpm.test:/rpm.test \
+      fedora:29 bash /rpm.test
+
+    echo
+    echo
+    echo "Testing rpm package (Amazon Corretto 11, 64bit)"
+    docker run -i --rm \
+      -v ${RPM64}:/apacheds.rpm \
+      -v ${TEST_SCRIPTS_DIR}/rpm.test:/rpm.test \
+      amazoncorretto:11 bash /rpm.test
+fi
