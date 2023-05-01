5f25878d9583c92c3784d6fe431ac18a9a405838
emmanuel lecharny <elecharny@apache.org>
2023-05-01 08:14:28 +0200

    remove useless imports

--- interceptors/subtree/src/test/java/org/apache/directory/server/core/subtree/RefinementLeafEvaluatorTest.java
+++ interceptors/subtree/src/test/java/org/apache/directory/server/core/subtree/RefinementLeafEvaluatorTest.java
@@ -40,8 +40,6 @@ import org.apache.directory.server.core.api.subtree.RefinementLeafEvaluator;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 
 
 /**
